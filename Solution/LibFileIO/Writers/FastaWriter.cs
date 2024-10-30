7506df8a07f64bef1e8364f184178849f932566a
eeoooue <pdmoorhouse@gmail.com>
2024-10-30 22:45:58 +0000

    scaffolded a FastaWriter class

---
+++ Solution/LibFileIO/Writers/FastaWriter.cs
@@ -0,0 +1,27 @@
+﻿using LibBioInfo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibFileIO.Writers
+{
+    public class FastaWriter : IAlignmentWriter
+    {
+        public void WriteAlignmentTo(Alignment alignment, string filename)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<string> CreateWriteableLines(Alignment alignment)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<string> CreateSequenceLines(BioSequence sequence)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
