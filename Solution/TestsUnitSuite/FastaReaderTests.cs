c7f1fcd708ec2c4d2eaa7d6b07e56a2739117141
eeoooue <pdmoorhouse@gmail.com>
2024-10-30 17:50:48 +0000

    created a FASTA parse test for sequence ExampleA

--- Solution/TestsUnitSuite/FastaReaderTests.cs
+++ Solution/TestsUnitSuite/FastaReaderTests.cs
@@ -1,26 +1,54 @@
+using LibBioInfo;
+using LibFileIO.Readers;
+using TestsUnitSuite.Harness;
+
 namespace TestsUnitSuite
 {
     [TestClass]
     public class FastaReaderTests
     {
+        private FastaReader FastaReader = new FastaReader();
+        private ExampleSequences ExampleSequences = new ExampleSequences();
+        private TestingHarness Harness = new TestingHarness();
+
         [TestMethod]
         public void CanParseSequenceA()
         {
+            List<string> contents = new List<string>()
+            {
+                ">ExampleA",
+                "ACGTACGTACGTACGTACGT",
+                "",
+            };
+
+            BioSequence expected = ExampleSequences.GetSequence(ExampleSequence.ExampleA);
+            BioSequence actual = FastaReader.ParseAsSequence(contents);
+
+            Assert.AreEqual(expected.Identifier, actual.Identifier);
+            Assert.AreEqual(expected.Payload, actual.Payload);
         }
 
         [TestMethod]
         public void CanParseSequenceB()
         {
+
+
         }
 
         [TestMethod]
         public void CanParseSequenceC()
         {
+
+
+
         }
 
         [TestMethod]
         public void CanParseSequenceD()
         {
+
+
+
         }
     }
 }
