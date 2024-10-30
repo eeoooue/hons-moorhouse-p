f536b5939601daa59ed476c44f1eaa1d12e9f858
eeoooue <pdmoorhouse@gmail.com>
2024-10-30 18:47:30 +0000

    added method names from design

--- Solution/LibBioInfo/Alignment.cs
+++ Solution/LibBioInfo/Alignment.cs
@@ -15,6 +15,44 @@
             State = new bool[Height,Width];
         }
 
+        #region methods to be implemented
+
+        public void ContainsNucleicsOnly()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ContainsProteinsOnly()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetAlignedSequences()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetCharacterAt()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetColumn()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InitializeAlignmentState()
+        {
+            throw new NotImplementedException();
+        }
+        public void SequencesCanBeAligned()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
         private int DecideWidth()
         {
             // placeholder logic - alignment width is double the length of the longest sequence
