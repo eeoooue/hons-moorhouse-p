a3958bc61f62dcd3cf1096cdbfc18eaeb341aa92
eeoooue <pdmoorhouse@gmail.com>
2024-10-30 17:13:57 +0000

    scaffolded SequenceReader behaviour

--- Solution/LibFileIO/SequenceReader.cs
+++ Solution/LibFileIO/SequenceReader.cs
@@ -8,5 +8,21 @@ namespace LibFileIO
 {
     internal class SequenceReader
     {
+        public string Directory = "";
+
+        public List<int> ReadSequencesFrom(string filename)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<int> CollectIdentifierLocations(List<string> contents)
+        {
+           throw new NotImplementedException();
+        }
+
+        public List<int> ParseAsSequence(List<string> contents)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
