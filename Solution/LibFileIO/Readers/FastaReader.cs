956d66badb358d2f0cc6445c4360da539aa0d124
eeoooue <pdmoorhouse@gmail.com>
2024-10-30 17:31:12 +0000

    scaffolded logic for a FastaReader

--- Solution/LibFileIO/Readers/FastaReader.cs
+++ Solution/LibFileIO/Readers/FastaReader.cs
@@ -14,7 +14,11 @@ namespace LibFileIO.Readers
         public List<BioSequence> ReadSequencesFrom(string filename)
         {
             List<string> contents = File.ReadAllLines(filename).ToList();
+            return UnpackSequences(contents);
+        }
 
+        public List<BioSequence> UnpackSequences(List<string> contents)
+        {
             List<int> identifierIndexes = CollectIdentifierLocations(contents);
             identifierIndexes.Add(contents.Count); // including last line as an end point
 
@@ -34,12 +38,32 @@ namespace LibFileIO.Readers
 
         public List<int> CollectIdentifierLocations(List<string> contents)
         {
-            throw new NotImplementedException();
+            List<int> result = new List<int>();
+
+            for(int i=0; i< contents.Count; i++)
+            {
+                if (contents[i].StartsWith(">"))
+                {
+                    result.Add(i);
+                }
+            }
+
+            return result;
         }
 
         public BioSequence ParseAsSequence(List<string> contents)
         {
-            throw new NotImplementedException();
+            string identifier = contents[0].Substring(1);
+
+            StringBuilder sb = new StringBuilder();
+            for(int i=1; i< contents.Count; i++)
+            {
+               sb.Append(contents[i]);
+            }
+
+            string payload = sb.ToString();
+
+            return new BioSequence(identifier, payload);
         }
     }
 }
