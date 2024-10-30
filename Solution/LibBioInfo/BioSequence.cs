82be55ac940dd7da1e586d91e5bb35d14f25388b
eeoooue <pdmoorhouse@gmail.com>
2024-10-30 17:31:05 +0000

    BioSequence has an identifier and payload

--- Solution/LibBioInfo/BioSequence.cs
+++ Solution/LibBioInfo/BioSequence.cs
@@ -8,5 +8,13 @@ namespace LibBioInfo
 {
     public class BioSequence
     {
+        public string Identifier { get; private set; }
+        public string Payload { get; private set; }
+
+        public BioSequence(string identifier, string payload)
+        {
+            Identifier = identifier;
+            Payload = payload;
+        }
     }
 }
