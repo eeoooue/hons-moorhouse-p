fadcee8d171cc34d5765c3091db5a6291de678e1
eeoooue <pdmoorhouse@gmail.com>
2024-10-30 19:11:05 +0000

    scaffolded early logic for BioSequence

--- Solution/LibBioInfo/BioSequence.cs
+++ Solution/LibBioInfo/BioSequence.cs
@@ -10,11 +10,61 @@ namespace LibBioInfo
     {
         public string Identifier { get; private set; }
         public string Payload { get; private set; }
+        public string Residues { get; private set; }
+
+        private static HashSet<char> AllowedNucleicResidues = new HashSet<char> { 'A', 'C', 'G', 'T', 'U' };
+        private static HashSet<char> AllowedProteinResidues = new HashSet<char> { 'A', 'C', 'G', 'T', 'U' };
 
         public BioSequence(string identifier, string payload)
         {
             Identifier = identifier;
-            Payload = payload;
+            Payload = payload.ToUpper();
+            Residues = ExtractResidues();
+        }
+
+        public bool IsNucleic()
+        {
+            foreach(char c in Residues)
+            {
+                if (!AllowedNucleicResidues.Contains(c))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public bool IsProtein()
+        {
+            foreach (char c in Residues)
+            {
+                if (!AllowedProteinResidues.Contains(c))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private string ExtractResidues()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach(char c in Payload)
+            {
+                if (char.IsLetter(c))
+                {
+                    sb.Append(c);
+                }
+            }
+
+            return sb.ToString();
+        }
+
+        public bool PayloadIsValid()
+        {
+            return IsNucleic() || IsProtein();
         }
     }
 }
