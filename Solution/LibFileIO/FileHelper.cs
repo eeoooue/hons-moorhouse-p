81e047f65b9b8b2dab7ae08284e9f184c676915c
eeoooue <pdmoorhouse@gmail.com>
2024-10-30 17:11:39 +0000

    scaffolded FileHelper behaviour

--- Solution/LibFileIO/FileHelper.cs
+++ Solution/LibFileIO/FileHelper.cs
@@ -1,7 +1,20 @@
-﻿namespace LibFileIO
+﻿using LibBioInfo;
+
+namespace LibFileIO
 {
     public class FileHelper
     {
+        private SequenceReader Reader = new SequenceReader();
+        private AlignmentWriter Writer = new AlignmentWriter();
 
+        public List<BioSequence> ReadSequencesFrom(string filename)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WriteAlignmentTo(Alignment alignment, string filename)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
