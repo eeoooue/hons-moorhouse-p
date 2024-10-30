482b899a8c2145d23ac4563ffaff7d57e5a15db4
eeoooue <pdmoorhouse@gmail.com>
2024-10-30 18:45:06 +0000

    added Alignment width & height logic, added a bool table for alignment state

--- Solution/LibBioInfo/Alignment.cs
+++ Solution/LibBioInfo/Alignment.cs
@@ -3,5 +3,30 @@
     public class Alignment
     {
         public List<BioSequence> Sequences;
+        public int Height { get { return Sequences.Count; } }
+        public int Width { get; private set; } = 0;
+
+        public bool[,] State;
+
+        public Alignment(List<BioSequence> sequences)
+        {
+            Sequences = sequences;
+            Width = DecideWidth();
+            State = new bool[Height,Width];
+        }
+
+        private int DecideWidth()
+        {
+            // placeholder logic - alignment width is double the length of the longest sequence
+
+            int width = 0;
+            foreach (BioSequence seq in Sequences)
+            {
+                int doubledWidth = seq.Payload.Length * 2;
+                width = Math.Max(width, doubledWidth);
+            }
+
+            return width;
+        }
     }
 }
