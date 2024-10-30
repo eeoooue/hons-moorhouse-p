d2e3e8ca4238d6ffd3b6bc142c8d9b8d3ded4e2a
eeoooue <pdmoorhouse@gmail.com>
2024-10-30 22:24:52 +0000

    refactoring test harness

--- Solution/TestsUnitSuite/HarnessTools/ExampleSequences.cs
+++ Solution/TestsUnitSuite/HarnessTools/ExampleSequences.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TestsUnitSuite.Harness
+namespace TestsUnitSuite.HarnessTools
 {
     internal enum ExampleSequence
     {
