71815d5584e88680f16144f9c2c6875178ba1c66
eeoooue <pdmoorhouse@gmail.com>
2024-10-30 17:45:01 +0000

    added some example sequences for testing

---
+++ Solution/TestsUnitSuite/FastaReaderTests.cs
@@ -0,0 +1,26 @@
+namespace TestsUnitSuite
+{
+    [TestClass]
+    public class FastaReaderTests
+    {
+        [TestMethod]
+        public void CanParseSequenceA()
+        {
+        }
+
+        [TestMethod]
+        public void CanParseSequenceB()
+        {
+        }
+
+        [TestMethod]
+        public void CanParseSequenceC()
+        {
+        }
+
+        [TestMethod]
+        public void CanParseSequenceD()
+        {
+        }
+    }
+}
