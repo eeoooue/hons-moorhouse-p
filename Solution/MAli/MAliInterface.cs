557b55ded1cc63912f8dc69d9bedb5a1c016ac2f
eeoooue <pdmoorhouse@gmail.com>
2024-11-01 20:04:23 +0000

    scaffolded early MAliInterface logic

--- Solution/MAli/MAliInterface.cs
+++ Solution/MAli/MAliInterface.cs
@@ -12,41 +12,96 @@ namespace MAli
 
         public void ProcessArguments(string[] args)
         {
+            Dictionary<string, string?> table = InterpretArguments(args);
 
-            if (IsAlignmentRequest(args))
+            if (IsAlignmentRequest(table))
             {
-                Facade.PerformAlignment(args);
+                Facade.PerformAlignment(table["input"]!, table["output"]!);
             }
 
-            if (IsHelpRequest(args))
+            if (IsHelpRequest(table))
             {
-                Facade.ProvideHelp(args);
+                Facade.ProvideHelp();
             }
 
-            if (IsInfoRequest(args))
+            if (IsInfoRequest(table))
             {
-                Facade.ProvideInfo(args);
+                Facade.ProvideInfo();
             }
         }
 
-        public bool ContainsForeignCommands(string[] args)
+        public Dictionary<string, string?> InterpretArguments(string[] args)
         {
+            Dictionary<string, string?> table = new Dictionary<string, string?>();
+
+            for(int i=0; i < args.Length; i++)
+            {
+                if (IsCommand(args[i]))
+                {
+                    string command = args[i].Substring(1);
+                    string? argument = null;
+
+                    if (i < args.Length - 1)
+                    {
+                        if (IsArgument(args[i + 1])){
+                            argument = args[i + 1];
+                        }
+                    }
+                    table[command] = argument;
+                }
+            }
+
+            return table;
+        }
+
+        public bool ContainsForeignCommands(Dictionary<string, string?> table)
+        {
+            MAliSpecification spec = new MAliSpecification();
+
+            foreach(string command in table.Keys)
+            {
+                if (!spec.SupportedCommands.Contains(command))
+                {
+                    return true;
+                }
+            }
+
             return false;
         }
 
-        public bool IsAlignmentRequest(string[] args)
+        public bool IsAlignmentRequest(Dictionary<string, string?> table)
         {
+            if (table.ContainsKey("input") && table.ContainsKey("output"))
+            {
+                if (table["input"] is string && table["output"] is string)
+                {
+                    return true;
+                }
+            }
+
             return false;
         }
 
-        public bool IsInfoRequest(string[] args)
+        public bool IsInfoRequest(Dictionary<string, string?> table)
         {
-            return false;
+            return table.ContainsKey("info");
         }
 
-        public bool IsHelpRequest(string[] args)
+        public bool IsHelpRequest(Dictionary<string, string?> table)
         {
-            return false;
+            return table.ContainsKey("help");
         }
+
+        private bool IsCommand(string candidate)
+        {
+            return candidate.StartsWith('-');
+        }
+
+        private bool IsArgument(string candidate)
+        {
+            return !candidate.StartsWith('-');
+        }
+
+        
     }
 }
