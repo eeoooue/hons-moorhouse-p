28184c4c9e6462a292d58fa0393ebf32ce5c8bc1
eeoooue <pdmoorhouse@gmail.com>
2024-11-01 20:04:05 +0000

    MAliSpecification can list version & support commands

--- Solution/MAli/MAliSpecification.cs
+++ Solution/MAli/MAliSpecification.cs
@@ -8,7 +8,8 @@ namespace MAli
 {
     internal class MAliSpecification
     {
-        HashSet<string> SupportedCommands = new HashSet<string>();
+        public HashSet<string> SupportedCommands = new HashSet<string>();
+        public string Version = "v0.1";
 
         public MAliSpecification()
         {
@@ -21,6 +22,21 @@ namespace MAli
             SupportedCommands.Add("output");
             SupportedCommands.Add("help");
             SupportedCommands.Add("info");
+        }
+
+        public void ListCurrentVersion()
+        {
+            Console.WriteLine($"MAli ({Version}) - Metaheuristic Aligner");
+        }
+
+        public void ListSupportedCommands()
+        {
+            Console.WriteLine("Supported commands:");
+            foreach (string command in SupportedCommands)
+            {
+                Console.WriteLine($"-{command}");
+            }
+            Console.WriteLine();
         }
     }
 }
