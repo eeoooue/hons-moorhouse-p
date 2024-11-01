b12d37bd482250b6f596acd2724784efa1aefe90
eeoooue <pdmoorhouse@gmail.com>
2024-11-01 19:38:00 +0000

    created MAliSpecification to house supported commands

---
+++ Solution/MAli/MAliSpecification.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAli
+{
+    internal class MAliSpecification
+    {
+        HashSet<string> SupportedCommands = new HashSet<string>();
+
+        public MAliSpecification()
+        {
+            AddSupportedCommands();
+        }
+
+        public void AddSupportedCommands()
+        {
+            SupportedCommands.Add("input");
+            SupportedCommands.Add("output");
+            SupportedCommands.Add("help");
+            SupportedCommands.Add("info");
+        }
+    }
+}
