bbe0b5c5cfecfde0ec9d734d2915f2c65a29498a
eeoooue <pdmoorhouse@gmail.com>
2024-11-01 20:04:14 +0000

    scaffolded a MAli facade

---
+++ Solution/MAli/MAliFacade.cs
@@ -0,0 +1,36 @@
+﻿using LibBioInfo;
+using LibFileIO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAli
+{
+    internal class MAliFacade
+    {
+        private MAliSpecification Specification = new MAliSpecification();
+        private FileHelper FileHelper = new FileHelper();
+
+        public void PerformAlignment(string inputPath, string outputPath)
+        {
+            List<BioSequence> sequences = FileHelper.ReadSequencesFrom(inputPath);
+            Alignment alignment = new Alignment(sequences);
+            FileHelper.WriteAlignmentTo(alignment, outputPath);
+        }
+
+        public void ProvideHelp()
+        {
+            Specification.ListCurrentVersion();
+            Specification.ListSupportedCommands();
+        }
+
+        public void ProvideInfo()
+        {
+            Specification.ListCurrentVersion();
+            Console.WriteLine($"For directions try 'readme.txt' or use '-help'");
+        }
+        
+    }
+}
