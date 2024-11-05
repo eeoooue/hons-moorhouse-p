cf80bde83552e33e3e77f4f2f16777972c8f88c5
eeoooue <pdmoorhouse@gmail.com>
2024-11-05 22:05:03 +0000

    Randomizer is the lone source of randomness for LibBioInfo

---
+++ Solution/LibBioInfo/Randomizer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibBioInfo
+{
+    public static class Randomizer
+    {
+        public static Random Random = new Random();
+    }
+}
