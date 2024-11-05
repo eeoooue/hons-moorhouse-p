b06832f82b15b2876f23f73211c0da484e36166a
eeoooue <pdmoorhouse@gmail.com>
2024-11-05 22:04:23 +0000

    created IAlignmentModifier interface

---
+++ Solution/LibBioInfo/IAlignmentModifier.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibBioInfo
+{
+    public interface IAlignmentModifier
+    {
+        public void ModifyAlignment(Alignment alignment);
+    }
+}
