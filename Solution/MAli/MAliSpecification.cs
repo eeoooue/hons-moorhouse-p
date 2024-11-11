36688ae14955b61c3c13e5bbd16007e2b49687a0
eeoooue <pdmoorhouse@gmail.com>
2024-11-11 23:37:57 +0000

    removed help command

--- Solution/MAli/MAliSpecification.cs
+++ Solution/MAli/MAliSpecification.cs
@@ -23,7 +23,6 @@ namespace MAli
             SupportedCommands.Add("input");
             SupportedCommands.Add("output");
             SupportedCommands.Add("help");
-            SupportedCommands.Add("info");
         }
 
         public void AddCommandDescriptions()
@@ -31,7 +30,6 @@ namespace MAli
             CommandDescriptions.Add("input", "Specify the input file of sequences to be aligned.");
             CommandDescriptions.Add("output", "Specify the output file path for the alignment of sequences.");
             CommandDescriptions.Add("help", "Display the list of supported commands.");
-            CommandDescriptions.Add("info", "Display the current version and additional information.");
         }
 
         public void ListCurrentVersion()
