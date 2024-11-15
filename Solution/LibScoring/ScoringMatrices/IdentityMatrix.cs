bcac46c1e8609c69ce1622a8f4f073f75e0f2e9d
eeoooue <pdmoorhouse@gmail.com>
2024-11-15 15:38:45 +0000

    IdentityMatrix is a ScoringMatrix

--- Solution/LibScoring/ScoringMatrices/IdentityMatrix.cs
+++ Solution/LibScoring/ScoringMatrices/IdentityMatrix.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace LibScoring.ScoringMatrices
 {
-    public class IdentityMatrix
+    public class IdentityMatrix : ScoringMatrix
     {
         public int ScorePair(char a, char b)
         {
--- Solution/LibScoring/ScoringMatrix.cs
+++ Solution/LibScoring/ScoringMatrix.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace LibScoring
 {
-    internal class ScoringMatrix
+    public abstract class ScoringMatrix
     {
     }
 }
