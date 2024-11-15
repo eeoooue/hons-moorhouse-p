8b372e2ac383e47c62ab6b335ac4b8892464ae6d
eeoooue <pdmoorhouse@gmail.com>
2024-11-15 15:39:07 +0000

    SOP OF can be instantiated

---
+++ Solution/LibScoring/ObjectiveFunctions/SumOfPairsObjectiveFunction.cs
@@ -0,0 +1,25 @@
+﻿using LibBioInfo;
+using LibScoring.ScoringMatrices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibScoring.ObjectiveFunctions
+{
+    public class SumOfPairsObjectiveFunction : IObjectiveFunction
+    {
+        ScoringMatrix Matrix;
+
+        public SumOfPairsObjectiveFunction(ScoringMatrix matrix)
+        {
+            Matrix = matrix;
+        }
+
+        public double ScoreAlignment(Alignment alignment)
+        {
+            return 0;
+        }
+    }
+}
---
+++ Solution/TestsUnitSuite/LibScoring/ObjectiveFunctions/SumOfPairsObjectiveTests.cs
@@ -0,0 +1,22 @@
+﻿using LibScoring;
+using LibScoring.ObjectiveFunctions;
+using LibScoring.ScoringMatrices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestsUnitSuite.LibScoring.ObjectiveFunctions
+{
+    [TestClass]
+    public class SumOfPairsObjectiveTests
+    {
+        [TestMethod]
+        public void CanInstantiateObjective()
+        {
+            ScoringMatrix matrix = new IdentityMatrix();
+            IObjectiveFunction function = new SumOfPairsObjectiveFunction(matrix);
+        }
+    }
+}
