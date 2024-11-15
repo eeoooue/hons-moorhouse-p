df3e1643cc852df13b8622b48eb7ae72177e7071
eeoooue <pdmoorhouse@gmail.com>
2024-11-15 15:31:43 +0000

    IObjectiveFunction specifies ScoreAlignment

--- Solution/LibScoring/IObjectiveFunction.cs
+++ Solution/LibScoring/IObjectiveFunction.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LibBioInfo;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,6 @@ namespace LibScoring
 {
     public interface IObjectiveFunction
     {
+        public double ScoreAlignment(Alignment alignment);
     }
 }
