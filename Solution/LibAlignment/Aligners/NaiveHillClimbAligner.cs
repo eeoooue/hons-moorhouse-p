8c4a826f8dc2a4de602d4cdf8cd53384aa0f53e1
eeoooue <pdmoorhouse@gmail.com>
2024-11-20 13:48:50 +0000

    scaffolded a naive hill climb aligner (uses stochastic row neighbours)

---
+++ Solution/LibAlignment/Aligners/NaiveHillClimbAligner.cs
@@ -0,0 +1,75 @@
+﻿using LibBioInfo;
+using LibBioInfo.IAlignmentModifiers;
+using LibBioInfo.INeighbourhoodFinders;
+using LibScoring;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibAlignment.Aligners
+{
+    internal class NaiveHillClimbAligner : Aligner
+    {
+        public NaiveHillClimbAligner(IObjectiveFunction objective, int iterations) : base(objective, iterations)
+        {
+        }
+
+        public override Alignment AlignSequences(List<BioSequence> sequences)
+        {
+            Initialize(sequences);
+            while (IterationsCompleted < IterationsLimit)
+            {
+                Iterate();
+                IterationsCompleted++;
+            }
+
+            return CurrentAlignment!;
+        }
+
+        public override void Initialize(List<BioSequence> sequences)
+        {
+            CurrentAlignment = new Alignment(sequences);
+            IAlignmentModifier randomizer = new AlignmentRandomizer();
+            randomizer.ModifyAlignment(CurrentAlignment);
+            IterationsCompleted = 0;
+            AlignmentScore = ScoreAlignment(CurrentAlignment);
+        }
+
+        public void AcceptIfImprovement(Alignment candidate)
+        {
+            double score = ScoreAlignment(candidate);
+            if (score > AlignmentScore)
+            {
+                CurrentAlignment = candidate;
+                AlignmentScore = score;
+            }
+        }
+
+        public override void Iterate()
+        {
+            foreach(Alignment candidate in GetNeighbouringAlignments(CurrentAlignment!))
+            {
+                AcceptIfImprovement(candidate);
+            }
+        }
+
+        public List<Alignment> GetNeighbouringAlignments(Alignment alignment)
+        {
+            List<Alignment> result = new List<Alignment>();
+
+            INeighbourhoodFinder finder = new RowBasedNeighbourhoodFinder();
+
+            List<bool[,]> neighbouringStates = finder.FindNeighbours(alignment.State);
+
+            foreach (bool[,] state in neighbouringStates)
+            {
+                Alignment neighbour = alignment.GetCopy();
+                neighbour.State = state;
+            }
+
+            return result;
+        }
+    }
+}
