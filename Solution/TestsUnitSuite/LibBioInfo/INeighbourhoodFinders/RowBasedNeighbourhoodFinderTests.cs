3b7238057ddd5cfbe62a0370f3061bf0c0de6705
eeoooue <pdmoorhouse@gmail.com>
2024-11-20 12:12:14 +0000

    tests for rowbased swaps

---
+++ Solution/TestsUnitSuite/LibBioInfo/INeighbourhoodFinders/RowBasedNeighbourhoodFinderTests.cs
@@ -0,0 +1,35 @@
+﻿using LibBioInfo.INeighbourhoodFinders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestsUnitSuite.HarnessTools;
+
+namespace TestsUnitSuite.LibBioInfo.INeighbourhoodFinders
+{
+    [TestClass]
+    public class RowBasedNeighbourhoodFinderTests
+    {
+        public RowBasedNeighbourhoodFinder Finder = new RowBasedNeighbourhoodFinder();
+        StateEquality StateEquality = Harness.StateEquality;
+
+        [TestMethod]
+        public void CanMakeIJSwapInRowA()
+        {
+            bool[] before = { true, false, true, true };
+            bool[] expected = { false, true, true, true };
+            bool[] result = Finder.GetRowAfterSwap(before, 0, 1);
+            bool verdict = StateEquality.RowsMatch(expected, result);
+        }
+
+        [TestMethod]
+        public void CanMakeIJSwapInRowB()
+        {
+            bool[] before = { true, false, true, true };
+            bool[] expected = { true, true, false, true };
+            bool[] result = Finder.GetRowAfterSwap(before, 1, 0);
+            bool verdict = StateEquality.RowsMatch(expected, result);
+        }
+    }
+}
