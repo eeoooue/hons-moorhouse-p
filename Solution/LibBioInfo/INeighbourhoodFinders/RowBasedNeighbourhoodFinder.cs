6dc4a0e86f9e41bc80a18d31f5f1ffc26fd8e670
eeoooue <pdmoorhouse@gmail.com>
2024-11-20 12:12:05 +0000

    starting implementation for RowBasedNeighbourhoodFinder

---
+++ Solution/LibBioInfo/INeighbourhoodFinders/RowBasedNeighbourhoodFinder.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibBioInfo.INeighbourhoodFinders
+{
+    public class RowBasedNeighbourhoodFinder : INeighbourhoodFinder
+    {
+        public List<bool[,]> FindNeighbours(bool[,] state)
+        {
+            int i = Randomizer.Random.Next(state.GetLength(0));
+            return FindNeighboursForRow(state, i);
+        }
+
+        public List<bool[,]> FindNeighboursForRow(bool[,] state, int i)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool[] GetStateWithReplacedRow(bool[,] state, int i, bool[] row)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<bool[]> GetNeighboursOfRow(bool[] row)
+        {
+            List<bool[]> result = new List<bool[]>();
+            for(int i=0; i<row.Length; i++)
+            {
+                for(int j=i+1; j<row.Length; j++)
+                {
+                    if (row[i] != row[j])
+                    {
+                        bool[] neighbour = GetRowAfterSwap(row, i, j);
+                        result.Append(neighbour);
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        public bool[] GetRowAfterSwap(bool[] original, int a, int b)
+        {
+            bool[] result = new bool[original.Length];
+            for(int i=0; i< original.Length; i++)
+            {
+                result[i] = original[i];
+            }
+            result[a] = original[b];
+            result[b] = original[a];
+
+            return result;
+        }
+    }
+}
