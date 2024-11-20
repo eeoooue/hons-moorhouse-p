6179dcc58d15eb7a5557f89d2164129376a04565
eeoooue <pdmoorhouse@gmail.com>
2024-11-20 12:11:40 +0000

    added StateEquality helper to Harness

---
+++ Solution/TestsUnitSuite/HarnessTools/StateEquality.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestsUnitSuite.HarnessTools
+{
+    internal class StateEquality
+    {
+
+        public bool RowsMatch(bool[] expected, bool[] actual)
+        {
+            if (expected.Length != actual.Length)
+            {
+                return false;
+            }
+
+            for(int i=0; i<expected.Length; i++)
+            {
+                if (expected[i] != actual[i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
