2466297eee565f0fca9e93754c80e1e07240f2af
eeoooue <pdmoorhouse@gmail.com>
2024-11-25 16:22:01 +0000

    created new experiment setup for s2-e2

---
+++ Experiments/Sprint02-Exp02/main.py
@@ -0,0 +1,18 @@
+
+import os
+from batch_scorer import BatchScorer
+from wrapped_subset import WrappedSubset
+from wrapped_aligner import WrappedAligner
+from wrapped_scorer import WrappedScorer
+
+subset = WrappedSubset("PREFAB-I")
+aligner = WrappedAligner("MAli Candidate", "MAli", "MAli-candidate")
+scorer = WrappedScorer("qscore")
+batch_scorer = BatchScorer(aligner, subset, scorer)
+
+SEED_VALUE = 2
+aligner.set_seed(SEED_VALUE)
+
+batch_scorer.record_scores(f"{aligner.title}_seed_{SEED_VALUE}.csv")
+
+# needs timers
