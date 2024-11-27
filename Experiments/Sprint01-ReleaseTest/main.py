c7791ce0b65378821704c2d68f43969f71c556b6
eeoooue <pdmoorhouse@gmail.com>
2024-11-27 12:43:37 +0000

    performed tests + analysis

--- Experiments/Sprint01-ReleaseTest/main.py
+++ Experiments/Sprint01-ReleaseTest/main.py
@@ -4,16 +4,15 @@ from wrapped_subset import WrappedSubset
 from wrapped_aligner import WrappedAligner
 from wrapped_scorer import WrappedScorer
 
-subset = WrappedSubset("PREFAB-B")
-# aligner = WrappedAligner("MAli v0.1", "MAli", "MAli-v0.1")
-aligner = WrappedAligner("MAli Candidate", "MAli", "MAli-candidate")
+subset = WrappedSubset("PREFAB-A")
+aligner = WrappedAligner("MAli v0.1", "MAli", "MAli-v0.1")
 
 scorer = WrappedScorer("qscore")
 batch_scorer = BatchScorer(aligner, subset, scorer)
 
-aligner.specify_iterations(10)
+# 1011, 1012, 1013, 1014, 1015
 
-SEED_VALUE = 102
+SEED_VALUE = 1015
 aligner.set_seed(SEED_VALUE)
 
 batch_scorer.record_scores(f"{aligner.title}_seed_{SEED_VALUE}.csv")
