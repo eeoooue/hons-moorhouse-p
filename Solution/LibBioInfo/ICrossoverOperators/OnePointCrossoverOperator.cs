1985b46d3ad69c2b350c94eb6e46e3170508cadf
eeoooue <pdmoorhouse@gmail.com>
2024-11-29 18:18:53 +0000

    scaffolded a 1point crossover operator

---
+++ Solution/LibBioInfo/ICrossoverOperators/OnePointCrossoverOperator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibBioInfo.ICrossoverOperators
+{
+    internal class OnePointCrossoverOperator : ICrossoverOperator
+    {
+
+        // based on One-Point Crossover operation described in SAGA (Notredame & Higgins, 1996)
+
+        public Alignment CreateChildAlignment(Alignment a, Alignment b)
+        {
+
+            // the alignment A is cut at a randomly chosen position
+
+            // complement segments are taken from B such that the resulting child is a valid state
+
+            int i = Randomizer.Random.Next(a.Width);
+            return CrossoverAtPosition(a, b, i);
+        }
+
+        public Alignment CrossoverAtPosition(Alignment a, Alignment b, int i)
+        {
+
+
+
+
+
+
+            throw new NotImplementedException();
+        }
+    }
+}
