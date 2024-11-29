07d1b534f423646bbf432d2c835073613b72ad2c
eeoooue <pdmoorhouse@gmail.com>
2024-11-29 18:18:43 +0000

    introduced CrossoverOperator interface

---
+++ Solution/LibBioInfo/ICrossoverOperator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibBioInfo
+{
+    public interface ICrossoverOperator
+    {
+        public Alignment CreateChildAlignment(Alignment a, Alignment b);
+
+    }
+}
