eda306e4486cfab3f2a5dfb2d8121b7ac1be29d9
eeoooue <pdmoorhouse@gmail.com>
2024-11-30 14:04:37 +0000

    added setup

--- Experiments/Sprint03-Exp01/main.py
+++ Experiments/Sprint03-Exp01/main.py
@@ -4,16 +4,16 @@ from wrapped_subset import WrappedSubset
 from wrapped_aligner import WrappedAligner
 from wrapped_scorer import WrappedScorer
 
-subset = WrappedSubset("PREFAB-I")
+subset = WrappedSubset("PREFAB-J")
 # aligner = WrappedAligner("MAli v0.1", "MAli", "MAli-v0.1")
 aligner = WrappedAligner("MAli Candidate", "MAli", "MAli-candidate")
 
 scorer = WrappedScorer("qscore")
 batch_scorer = BatchScorer(aligner, subset, scorer)
 
-aligner.specify_iterations(1000)
+aligner.specify_iterations(100)
 
-SEED_VALUE = 1756
+SEED_VALUE = 525
 aligner.set_seed(SEED_VALUE)
 
 batch_scorer.record_scores(f"{aligner.title}_seed_{SEED_VALUE}.csv")
