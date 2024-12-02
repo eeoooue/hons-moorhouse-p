e36054c1d67f952acbaf6b515078e042fc89ae9e
eeoooue <pdmoorhouse@gmail.com>
2024-12-02 21:17:31 +0000

    moved some performance sum of pairs scoring tests across into performance suite

--- Solution/TestsUnitSuite/LibScoring/ObjectiveFunctions/SumOfPairsObjectiveTests.cs
+++ Solution/TestsUnitSuite/LibScoring/ObjectiveFunctions/SumOfPairsObjectiveTests.cs
@@ -21,50 +21,6 @@ namespace TestsUnitSuite.LibScoring.ObjectiveFunctions
         ExampleAlignments ExampleAlignments = Harness.ExampleAlignments;
         FileHelper FileHelper = new FileHelper();
 
-        #region Is Time Efficient
-
-        [DataTestMethod]
-        [DataRow("BB11001", 8)]
-        [DataRow("BB11001", 16)]
-        [DataRow("BB11001", 32)]
-        [Timeout(500)]
-        public void CanScoreBBSAlignmentsEfficiently(string filename, int times)
-        {
-            List<Alignment> result = new List<Alignment>();
-
-            List<BioSequence> sequences = FileHelper.ReadSequencesFrom(filename);
-            Alignment alignment = new Alignment(sequences);
-
-            for (int i = 0; i < times; i++)
-            {
-                IScoringMatrix matrix = new IdentityMatrix();
-                IObjectiveFunction function = new SumOfPairsObjectiveFunction(matrix);
-                double score = function.ScoreAlignment(alignment);
-            }
-        }
-
-        [DataTestMethod]
-        [DataRow("1ggxA_1h4uA", 8)]
-        [DataRow("1ggxA_1h4uA", 16)]
-        [DataRow("1ggxA_1h4uA", 32)]
-        [Timeout(5000)]
-        public void CanScorePREFABAlignmentsEfficiently(string filename, int times)
-        {
-            List<Alignment> result = new List<Alignment>();
-
-            List<BioSequence> sequences = FileHelper.ReadSequencesFrom(filename);
-            Alignment alignment = new Alignment(sequences);
-            IScoringMatrix matrix = new IdentityMatrix();
-            IObjectiveFunction function = new SumOfPairsObjectiveFunction(matrix);
-
-            for (int i = 0; i < times; i++)
-            {
-                double score = function.ScoreAlignment(alignment);
-            }
-        }
-
-        #endregion
-
         [TestMethod]
         public void CanInstantiateObjectiveWithIdentityMatrix()
         {
