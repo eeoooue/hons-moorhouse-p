b7f9457e7ccd5823eb3c0d006d3d4c6df279b78d
eeoooue <pdmoorhouse@gmail.com>
2024-12-02 13:11:24 +0000

    implemented some helper methods for AffineGapPenalty OF

--- Solution/LibScoring/ObjectiveFunctions/AffineGapPenaltyObjectiveFunction.cs
+++ Solution/LibScoring/ObjectiveFunctions/AffineGapPenaltyObjectiveFunction.cs
@@ -9,10 +9,12 @@ namespace LibScoring.ObjectiveFunctions
 {
     public class AffineGapPenaltyObjectiveFunction : IObjectiveFunction
     {
-        public double OpeningCost { get; private set; } = 4;
-        public double NullCost { get; private set; } = 1;
+        public double OpeningCost { get; private set; } 
+        public double NullCost { get; private set; }
 
-        public AffineGapPenaltyObjectiveFunction(double openingCost, double nullCost)
+        private static Bioinformatics Bioinformatics = new Bioinformatics();
+
+        public AffineGapPenaltyObjectiveFunction(double openingCost=4, double nullCost=1)
         {
             OpeningCost = openingCost;
             NullCost = nullCost;
@@ -25,7 +27,55 @@ namespace LibScoring.ObjectiveFunctions
 
         public double ScorePayload(string payload)
         {
+            string trimmed = TrimPayload(payload);
+            List<int> gapSizes = CollectGapSizes(trimmed);
+
+
+
             throw new NotImplementedException();
+        }
+
+        public List<int> CollectGapSizes(string payload)
+        {
+            List<int> result = new List<int>();
+
+            return result;
+        }
+
+        public string TrimPayload(string payload)
+        {
+            int i = GetIndexOfFirstResidue(payload);
+            int j = GetIndexOfLastResidue(payload);
+            int length = 1 + j - i;
+            string trimmed = payload.Substring(i, length);
+
+            return trimmed;
+        }
+
+        public int GetIndexOfFirstResidue(string payload)
+        {
+            for(int i=0; i<payload.Length; i++)
+            {
+                if (!Bioinformatics.IsGapChar(payload[i]))
+                {
+                    return i;
+                }
+            }
+
+            return payload.Length;
+        }
+
+        public int GetIndexOfLastResidue(string payload)
+        {
+            for (int i = payload.Length-1; i >= 0; i--)
+            {
+                if (!Bioinformatics.IsGapChar(payload[i]))
+                {
+                    return i;
+                }
+            }
+
+            return payload.Length;
         }
     }
 }
