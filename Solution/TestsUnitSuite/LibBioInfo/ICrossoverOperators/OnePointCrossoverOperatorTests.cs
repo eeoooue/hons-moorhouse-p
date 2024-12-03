a51de25ac33d665163c1c02ca236812701af4e09
eeoooue <pdmoorhouse@gmail.com>
2024-12-03 19:48:48 +0000

    added tests for conservation of sequence data in crossover operations

--- Solution/TestsUnitSuite/LibBioInfo/ICrossoverOperators/OnePointCrossoverOperatorTests.cs
+++ Solution/TestsUnitSuite/LibBioInfo/ICrossoverOperators/OnePointCrossoverOperatorTests.cs
@@ -26,6 +26,32 @@ namespace TestsUnitSuite.LibBioInfo.ICrossoverOperators
 
         OnePointCrossoverOperator Operator = new OnePointCrossoverOperator();
 
+        [DataTestMethod]
+        [DataRow(1, 0)]
+        [DataRow(1, 1)]
+        [DataRow(12, 0)]
+        [DataRow(12, 1)]
+        [DataRow(123, 0)]
+        [DataRow(123, 1)]
+        [DataRow(1234, 0)]
+        [DataRow(1234, 1)]
+        [DataRow(12345, 0)]
+        [DataRow(12345, 1)]
+        public void CrossoverConservesSequenceData(int seed, int i)
+        {
+            Randomizer.SetSeed(seed);
+
+            Alignment a = ExampleAlignments.GetExampleA();
+            Alignment b = ExampleAlignments.GetExampleA();
+            IAlignmentModifier randomizer = new AlignmentRandomizer();
+            randomizer.ModifyAlignment(a);
+            randomizer.ModifyAlignment(b);
+
+            List<Alignment> children = Operator.CreateAlignmentChildren(a, b);
+            Alignment child = children[i];
+            AlignmentConservation.AssertAlignmentsAreConserved(a, child);
+        }
+
         [TestMethod]
         public void CanCrossoverAlignments()
         {
--- Solution/TestsUnitSuite/LibBioInfo/ICrossoverOperators/SAGAOnePointCrossoverOperatorTests.cs
+++ Solution/TestsUnitSuite/LibBioInfo/ICrossoverOperators/SAGAOnePointCrossoverOperatorTests.cs
@@ -22,8 +22,39 @@ namespace TestsUnitSuite.LibBioInfo.ICrossoverOperators
         AlignmentStateConverter AlignmentStateConverter = Harness.AlignmentStateConverter;
         StateEquality StateEquality = Harness.StateEquality;
         ExampleAlignments ExampleAlignments = Harness.ExampleAlignments;
+        AlignmentConservation AlignmentConservation = Harness.AlignmentConservation;
 
         SAGAOnePointCrossoverOperator Operator = new SAGAOnePointCrossoverOperator();
+
+
+
+        [DataTestMethod]
+        [DataRow(1, 0)]
+        [DataRow(1, 1)]
+        [DataRow(12, 0)]
+        [DataRow(12, 1)]
+        [DataRow(123, 0)]
+        [DataRow(123, 1)]
+        [DataRow(1234, 0)]
+        [DataRow(1234, 1)]
+        [DataRow(12345, 0)]
+        [DataRow(12345, 1)]
+        public void CrossoverConservesSequenceData(int seed, int i)
+        {
+            Randomizer.SetSeed(seed);
+
+            Alignment a = ExampleAlignments.GetExampleA();
+            Alignment b = ExampleAlignments.GetExampleA();
+            IAlignmentModifier randomizer = new AlignmentRandomizer();
+            randomizer.ModifyAlignment(a);
+            randomizer.ModifyAlignment(b);
+
+            List<Alignment> children = Operator.CreateAlignmentChildren(a, b);
+            Alignment child = children[i];
+            AlignmentConservation.AssertAlignmentsAreConserved(a, child);
+        }
+
+
 
         [TestMethod]
         public void CanCreateAlignmentChildren()
