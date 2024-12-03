fc7fb459a1e50d89074aff8511b2566167fd82c4
eeoooue <pdmoorhouse@gmail.com>
2024-12-03 20:02:53 +0000

    made an AlignmentStateHelper

---
+++ Solution/LibBioInfo/Helpers/AlignmentStateHelper.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibBioInfo.Helpers
+{
+    public class AlignmentStateHelper
+    {
+        public bool[,] CombineAlignmentStates(bool[,] leftState, bool[,] rightState)
+        {
+            int m = leftState.GetLength(0);
+            int leftN = leftState.GetLength(1);
+            int rightN = rightState.GetLength(1);
+
+            bool[,] result = new bool[m, leftN + rightN];
+
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < leftN; j++)
+                {
+                    result[i, j] = leftState[i, j];
+                }
+
+                for (int j = 0; j < rightN; j++)
+                {
+                    result[i, j + leftN] = rightState[i, j];
+                }
+            }
+
+            return result;
+        }
+
+        public int GetPositionOfNthResidue(bool[,] state, int i, int n)
+        {
+            int counter = 0;
+            for (int j = 0; j < state.GetLength(1); j++)
+            {
+                if (state[i, j] == false)
+                {
+                    counter++;
+                }
+
+                if (counter == n)
+                {
+                    return j;
+                }
+            }
+
+            return state.GetLength(1);
+        }
+
+        public int GetNumberOfResiduesInRow(bool[,] state, int i)
+        {
+            int result = 0;
+            for (int j = 0; j < state.GetLength(1); j++)
+            {
+                if (state[i, j] == false)
+                {
+                    result++;
+                }
+            }
+
+            return result;
+        }
+
+    }
+}
--- Solution/LibBioInfo/ICrossoverOperators/SAGAOnePointCrossoverOperator.cs
+++ Solution/LibBioInfo/ICrossoverOperators/SAGAOnePointCrossoverOperator.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LibBioInfo.Helpers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,8 @@ namespace LibBioInfo.ICrossoverOperators
 {
     public class SAGAOnePointCrossoverOperator : ICrossoverOperator
     {
+        private AlignmentStateHelper StateHelper = new AlignmentStateHelper();
+
         public List<Alignment> CreateAlignmentChildren(Alignment a, Alignment b)
         {
             int heads = Randomizer.Random.Next(0, 2);
@@ -39,7 +42,7 @@ namespace LibBioInfo.ICrossoverOperators
             List<int> bRightPositions = GetComplementPositionsForLeftState(aLeftState, b);
             bool[,] bRightState = CollectRightsUntilPositions(b.State, bRightPositions);
 
-            bool[,] childState = CombineAlignmentStates(aLeftState, bRightState);
+            bool[,] childState = StateHelper.CombineAlignmentStates(aLeftState, bRightState);
 
             Alignment child = a.GetCopy();
             child.State = childState;
@@ -54,7 +57,7 @@ namespace LibBioInfo.ICrossoverOperators
             List<int> bLeftPositions = GetComplementPositionsForRightState(aRightState, b);
             bool[,] bLeftState = CollectLeftsUntilPositions(b.State, bLeftPositions);
 
-            bool[,] childState = CombineAlignmentStates(bLeftState, aRightState);
+            bool[,] childState = StateHelper.CombineAlignmentStates(bLeftState, aRightState);
 
             Alignment child = a.GetCopy();
             child.State = childState;
@@ -62,29 +65,6 @@ namespace LibBioInfo.ICrossoverOperators
             return child;
         }
 
-        public bool[,] CombineAlignmentStates(bool[,] leftState, bool[,] rightState)
-        {
-            int m = leftState.GetLength(0);
-            int leftN = leftState.GetLength(1);
-            int rightN = rightState.GetLength(1);
-
-            bool[,] result = new bool[m, leftN + rightN];
-
-            for(int i=0; i<m; i++)
-            {
-                for(int j=0; j<leftN; j++)
-                {
-                    result[i, j] = leftState[i, j];
-                }
-                
-                for (int j = 0; j < rightN; j++)
-                {
-                    result[i, j + leftN] = rightState[i, j];
-                }
-            }
-
-            return result;
-        }
 
         public List<int> GetComplementPositionsForLeftState(bool[,] leftState, Alignment source)
         {
@@ -92,8 +72,8 @@ namespace LibBioInfo.ICrossoverOperators
 
             for(int i=0; i<source.Height; i++)
             {
-                int n = GetNumberOfResiduesInRow(leftState, i);
-                int position = GetPositionOfNthResidue(source.State, i, n);
+                int n = StateHelper.GetNumberOfResiduesInRow(leftState, i);
+                int position = StateHelper.GetPositionOfNthResidue(source.State, i, n);
                 result.Add(position);
             }
 
@@ -106,49 +86,15 @@ namespace LibBioInfo.ICrossoverOperators
 
             for (int i = 0; i < source.Height; i++)
             {
-                int residuesInRightState = GetNumberOfResiduesInRow(rightState, i);
-                int totalResidues = GetNumberOfResiduesInRow(source.State, i);
+                int residuesInRightState = StateHelper.GetNumberOfResiduesInRow(rightState, i);
+                int totalResidues = StateHelper.GetNumberOfResiduesInRow(source.State, i);
                 int residuesToInclude = totalResidues - residuesInRightState;
-                int position = GetPositionOfNthResidue(source.State, i, residuesToInclude);
+                int position = StateHelper.GetPositionOfNthResidue(source.State, i, residuesToInclude);
                 result.Add(position+1);
             }
 
             return result;
         }
-
-        public int GetPositionOfNthResidue(bool[,] state, int i, int n)
-        {
-            int counter = 0;
-            for(int j=0; j<state.GetLength(1); j++)
-            {
-                if (state[i, j] == false)
-                {
-                    counter++;
-                }
-
-                if (counter == n)
-                {
-                    return j;
-                }
-            }
-
-            return state.GetLength(1);
-        }
-
-        public int GetNumberOfResiduesInRow(bool[,] state, int i)
-        {
-            int result = 0;
-            for(int j = 0; j < state.GetLength(1); j++)
-            {
-                if (state[i,j] == false)
-                {
-                    result++;
-                }
-            }
-
-            return result;
-        }
-
 
         public List<int> GetListOfPositions(int position, int count)
         {
@@ -281,88 +227,6 @@ namespace LibBioInfo.ICrossoverOperators
             return result;
         }
 
-        public bool[,] GetRightHandSide(Alignment alignment, bool[,] leftSide)
-        {
-            throw new NotImplementedException();
 
-
-            List<int> residuesToSkip = CountResiduesPerRow(leftSide);
-            List<int> starterPositions = CollectRightHandStarterPositions(alignment.State, residuesToSkip);
-
-            int leftMostPosition = alignment.Width - 1;
-            foreach(int x in starterPositions)
-            {
-                leftMostPosition = Math.Min(x, leftMostPosition);
-            }
-
-            int m = alignment.Height;
-
-            bool[,] result = new bool[m, alignment.Width - leftMostPosition];
-
-            for(int i=0; i<m; i++)
-            {
-                int starterPosition = starterPositions[i];
-
-                for(int j=starterPosition; j< alignment.Width; j++)
-                {
-                    int j2 = j - leftMostPosition;
-                    result[i, j2] = alignment.State[i, j];
-                }
-            }
-
-
-            return result;
-        }
-
-
-        public List<int> CollectRightHandStarterPositions(bool[,] state, List<int> residuesToSkip)
-        {
-            List<int> result = new List<int>();
-
-
-            for(int i=0; i<state.GetLength(0); i++)
-            {
-                int target = residuesToSkip[i];
-                int j = 0;
-
-                while (target > 0)
-                {
-                    if (state[i, j] == false)
-                    {
-                        target -= 1;
-                    }
-                    j += 1;
-                }
-
-                result.Add(j);
-
-            }
-
-            return result;
-
-        }
-
-
-        public List<int> CountResiduesPerRow(bool[,] state)
-        {
-            List<int> result = new List<int>();
-
-            for(int i=0; i<state.GetLength(0); i++)
-            {
-                int total = 0;
-
-                for(int j=0; j<state.GetLength(1); j++)
-                {
-                    if (state[i, j] == false)
-                    {
-                        total++;
-                    }
-                }
-
-                result.Add(total);
-            }
-
-            return result;
-        }
     }
 }
