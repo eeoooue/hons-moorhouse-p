d69c5aa1da45f2e9c8cd70e43a451a795c2f0144
eeoooue <pdmoorhouse@gmail.com>
2024-12-05 21:40:30 +0000

    Create AlignmentSelectionHelper.cs

---
+++ Solution/LibAlignment/Helpers/AlignmentSelectionHelper.cs
@@ -0,0 +1,31 @@
+﻿using LibBioInfo;
+using LibScoring;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibAlignment.Helpers
+{
+    public class AlignmentSelectionHelper
+    {
+        public List<Alignment> SelectFittestParents(List<ScoredAlignment> candidates, int parentsToSelect)
+        {
+            SortScoredAlignments(candidates);
+
+            List<Alignment> result = new List<Alignment>();
+            for (int i = 0; i < parentsToSelect; i++)
+            {
+                result.Add(candidates[i].Alignment);
+            }
+
+            return result;
+        }
+
+        public void SortScoredAlignments(List<ScoredAlignment> alignments)
+        {
+            alignments.Sort((a, b) => b.Score.CompareTo(a.Score));
+        }
+    }
+}
