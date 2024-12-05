4602b7f90d2dbb6fc0527c5ee33d50941a0851ed
eeoooue <pdmoorhouse@gmail.com>
2024-12-05 19:04:27 +0000

    scaffolded new EA mew lambda aligner

---
+++ Solution/LibAlignment/Aligners/MewLambdaEvolutionaryAlgorithmAligner.cs
@@ -0,0 +1,36 @@
+﻿using LibBioInfo.ICrossoverOperators;
+using LibBioInfo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibScoring;
+
+namespace LibAlignment.Aligners
+{
+    public class MewLambdaEvolutionaryAlgorithmAligner : Aligner
+    {
+        public List<Alignment> Population = new List<Alignment>();
+
+
+        public MewLambdaEvolutionaryAlgorithmAligner(IObjectiveFunction objective, int iterations) : base(objective, iterations)
+        {
+        }
+
+        public override Alignment AlignSequences(List<BioSequence> sequences)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Initialize(List<BioSequence> sequences)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Iterate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
