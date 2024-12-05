b85541c90e504c03ce1c473479e65e9838669184
eeoooue <pdmoorhouse@gmail.com>
2024-12-05 23:54:33 +0000

    Harness tool AlignmentPrinting offers state printing

--- Solution/TestsHarness/Tools/AlignmentPrinting.cs
+++ Solution/TestsHarness/Tools/AlignmentPrinting.cs
@@ -22,5 +22,30 @@ namespace TestsHarness.Tools
                 Console.WriteLine(payload);
             }
         }
+
+        public void PrintState(bool[,] state)
+        {
+            for(int i=0; i<state.GetLength(0); i++)
+            {
+                PrintStateRow(state, i);
+            }
+        }
+
+        public void PrintStateRow(bool[,] state, int i)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for(int j=0; j<state.GetLength(1); j++)
+            {
+                char x = 'X';
+                if (state[i, j])
+                {
+                    x = '-';
+                }
+                sb.Append(x);
+            }
+
+            Console.WriteLine(sb.ToString());
+        }
     }
 }
