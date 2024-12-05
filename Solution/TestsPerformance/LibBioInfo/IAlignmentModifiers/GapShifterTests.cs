d8f0a0a48d0c146cd1b54d8c71da46793745d279
eeoooue <pdmoorhouse@gmail.com>
2024-12-05 20:15:06 +0000

    moved performance tests into Performance Suite for GapShifter

---
+++ Solution/TestsPerformance/LibBioInfo/IAlignmentModifiers/GapShifterTests.cs
@@ -0,0 +1,72 @@
+﻿using LibBioInfo.IAlignmentModifiers;
+using LibBioInfo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestsHarness.Tools;
+using TestsHarness;
+using LibFileIO;
+
+namespace TestsPerformance.LibBioInfo.IAlignmentModifiers
+{
+    [TestClass]
+    public class GapShifterTests
+    {
+        ExampleSequences ExampleSequences = Harness.ExampleSequences;
+        ExampleAlignments ExampleAlignments = Harness.ExampleAlignments;
+        SequenceConservation SequenceConservation = Harness.SequenceConservation;
+        SequenceEquality SequenceEquality = Harness.SequenceEquality;
+        AlignmentEquality AlignmentEquality = Harness.AlignmentEquality;
+        AlignmentConservation AlignmentConservation = Harness.AlignmentConservation;
+
+        GapShifter GapShifter = new GapShifter();
+        FileHelper FileHelper = new FileHelper();
+
+        #region Is Time Efficient
+
+        [DataTestMethod]
+        [DataRow("BB11003", 8)]
+        [DataRow("BB11003", 16)]
+        [DataRow("BB11003", 32)]
+        [DataRow("BB11003", 64)]
+        [DataRow("BB11003", 128)]
+        [Timeout(500)]
+        public void CanModifyBBSAlignmentsEfficiently(string filename, int times)
+        {
+            List<Alignment> result = new List<Alignment>();
+
+            List<BioSequence> sequences = FileHelper.ReadSequencesFrom(filename);
+            Alignment alignment = new Alignment(sequences);
+
+            for (int i = 0; i < times; i++)
+            {
+                GapShifter.ModifyAlignment(alignment);
+            }
+        }
+
+        [DataTestMethod]
+        [DataRow("1ggxA_1h4uA", 8)]
+        [DataRow("1ggxA_1h4uA", 16)]
+        [DataRow("1ggxA_1h4uA", 32)]
+        [DataRow("1ggxA_1h4uA", 64)]
+        [DataRow("1ggxA_1h4uA", 128)]
+        [Timeout(500)]
+        public void CanModifyPREFABAlignmentsEfficiently(string filename, int times)
+        {
+            List<Alignment> result = new List<Alignment>();
+
+            List<BioSequence> sequences = FileHelper.ReadSequencesFrom(filename);
+            Alignment alignment = new Alignment(sequences);
+
+            for (int i = 0; i < times; i++)
+            {
+                GapShifter.ModifyAlignment(alignment);
+            }
+        }
+
+        #endregion
+
+    }
+}
--- Solution/TestsUnitSuite/LibBioInfo/IAlignmentModifiers/GapShifterTests.cs
+++ Solution/TestsUnitSuite/LibBioInfo/IAlignmentModifiers/GapShifterTests.cs
@@ -27,51 +27,6 @@ namespace TestsUnitSuite.LibBioInfo.IAlignmentModifiers
         AlignmentConservation AlignmentConservation = Harness.AlignmentConservation;
 
         GapShifter GapShifter = new GapShifter();
-        FileHelper FileHelper = new FileHelper();
-
-        #region Is Time Efficient
-
-        [DataTestMethod]
-        [DataRow("BB11003", 8)]
-        [DataRow("BB11003", 16)]
-        [DataRow("BB11003", 32)]
-        [DataRow("BB11003", 64)]
-        [DataRow("BB11003", 128)]
-        [Timeout(500)]
-        public void CanModifyBBSAlignmentsEfficiently(string filename, int times)
-        {
-            List<Alignment> result = new List<Alignment>();
-
-            List<BioSequence> sequences = FileHelper.ReadSequencesFrom(filename);
-            Alignment alignment = new Alignment(sequences);
-
-            for (int i = 0; i < times; i++)
-            {
-                GapShifter.ModifyAlignment(alignment);
-            }
-        }
-
-        [DataTestMethod]
-        [DataRow("1ggxA_1h4uA", 8)]
-        [DataRow("1ggxA_1h4uA", 16)]
-        [DataRow("1ggxA_1h4uA", 32)]
-        [DataRow("1ggxA_1h4uA", 64)]
-        [DataRow("1ggxA_1h4uA", 128)]
-        [Timeout(500)]
-        public void CanModifyPREFABAlignmentsEfficiently(string filename, int times)
-        {
-            List<Alignment> result = new List<Alignment>();
-
-            List<BioSequence> sequences = FileHelper.ReadSequencesFrom(filename);
-            Alignment alignment = new Alignment(sequences);
-
-            for (int i = 0; i < times; i++)
-            {
-                GapShifter.ModifyAlignment(alignment);
-            }
-        }
-
-        #endregion
 
         [TestMethod]
         public void AlignmentIsDifferentAfterShift()
