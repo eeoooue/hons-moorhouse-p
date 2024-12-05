d80a9ada41ea64467bc8dfac3537c321c72669c5
eeoooue <pdmoorhouse@gmail.com>
2024-12-05 20:40:43 +0000

    added functional + performance tests for PercentileGapShifter

---
+++ Solution/TestsUnitSuite/LibBioInfo/IAlignmentModifiers/PercentileGapShifterTests.cs
@@ -0,0 +1,46 @@
+﻿using LibBioInfo.IAlignmentModifiers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestsHarness.Tools;
+using TestsHarness;
+using LibBioInfo;
+
+namespace TestsUnitSuite.LibBioInfo.IAlignmentModifiers
+{
+    [TestClass]
+    public class PercentileGapShifterTests
+    {
+        ExampleAlignments ExampleAlignments = Harness.ExampleAlignments;
+        AlignmentEquality AlignmentEquality = Harness.AlignmentEquality;
+        AlignmentConservation AlignmentConservation = Harness.AlignmentConservation;
+
+        IAlignmentModifier PercentileGapShifter = new PercentileGapShifter(0.05);
+
+        [TestMethod]
+        public void AlignmentIsDifferentAfterShift()
+        {
+            Alignment original = ExampleAlignments.GetExampleA();
+            Alignment copy = original.GetCopy();
+            PercentileGapShifter.ModifyAlignment(copy);
+
+            bool alignmentsMatch = AlignmentEquality.AlignmentsMatch(original, copy);
+            Assert.IsFalse(alignmentsMatch);
+
+            AlignmentConservation.AssertAlignmentsAreConserved(copy, original);
+        }
+
+        [TestMethod]
+        public void AlignmentStateIsDifferentAfterShift()
+        {
+            Alignment original = ExampleAlignments.GetExampleA();
+            Alignment copy = original.GetCopy();
+            PercentileGapShifter.ModifyAlignment(copy);
+
+            bool alignmentsStatesMatch = AlignmentEquality.AlignmentsMatch(original, copy);
+            Assert.IsFalse(alignmentsStatesMatch);
+        }
+    }
+}
