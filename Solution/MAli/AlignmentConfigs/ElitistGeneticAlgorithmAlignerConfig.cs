956ab5418248553ef48ad237de8711be89c2d3a3
eeoooue <pdmoorhouse@gmail.com>
2024-12-06 14:17:30 +0000

    created ElitistGeneticAlgorithmAlignerConfig

---
+++ Solution/MAli/AlignmentConfigs/ElitistGeneticAlgorithmAlignerConfig.cs
@@ -0,0 +1,33 @@
+﻿using LibAlignment.Aligners;
+using LibAlignment;
+using LibScoring.ObjectiveFunctions;
+using LibScoring.ScoringMatrices;
+using LibScoring;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAli.AlignmentConfigs
+{
+    public class ElitistGeneticAlgorithmAlignerConfig : AlignmentConfig
+    {
+        public override Aligner CreateAligner()
+        {
+            return GetVersion01();
+        }
+
+        private Aligner GetVersion01()
+        {
+            IScoringMatrix matrix = new BLOSUM62Matrix();
+            IObjectiveFunction objective = new SumOfPairsWithAffineGapPenaltiesObjectiveFunction(matrix, 4, 1);
+            const int maxIterations = 100;
+            ElitistGeneticAlgorithmAligner aligner = new ElitistGeneticAlgorithmAligner(objective, maxIterations);
+            aligner.PopulationSize = 64;
+            aligner.SelectionSize = 16;
+
+            return aligner;
+        }
+    }
+}
