0e9b4b2e6db0ad0de19550f5f3a7c9a0d248ddf5
eeoooue <pdmoorhouse@gmail.com>
2024-12-06 14:47:52 +0000

    created ISelectionStrategy interface

---
+++ Solution/LibAlignment/ISelectionStrategy.cs
@@ -0,0 +1,17 @@
+﻿using LibBioInfo;
+using LibScoring;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibAlignment
+{
+    public interface ISelectionStrategy
+    {
+        public void PreprocessCandidateAlignments(List<ScoredAlignment> candidates);
+
+        public Alignment SelectCandidate();
+    }
+}
