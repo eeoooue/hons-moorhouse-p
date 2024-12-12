ef47ceb42db9b646aced00b60a2b59fa993f4280
eeoooue <pdmoorhouse@gmail.com>
2024-12-12 23:24:01 +0000

    scaffolded Linear Combination of Weights OF

---
+++ Solution/LibScoring/ObjectiveFunctions/LinearCombinationOfObjectiveFunctions.cs
@@ -0,0 +1,42 @@
+﻿using LibBioInfo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibScoring.ObjectiveFunctions
+{
+    public class LinearCombinationOfObjectiveFunctions : IObjectiveFunction
+    {
+        public List<IObjectiveFunction> Objectives;
+        public List<double> Weights;
+
+        public LinearCombinationOfObjectiveFunctions(List<IObjectiveFunction> objectives, List<double> weights)
+        {
+            Objectives = objectives;
+            Weights = weights;
+        }
+
+        public string GetName()
+        {
+            return "Linear Combination of Weights";
+        }
+
+        public double ScoreAlignment(Alignment alignment)
+        {
+            double result = 0;
+            for(int i=0; i<Objectives.Count; i++)
+            {
+                result += GetWeightedScore(alignment, Objectives[i], Weights[i]);
+            }
+
+            return result;
+        }
+
+        public double GetWeightedScore(Alignment alignment, IObjectiveFunction objective, double weight)
+        {
+            return objective.ScoreAlignment(alignment) * weight;
+        }
+    }
+}
