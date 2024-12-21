5f301001b9283996e2b4a893f296b908c113e1cf
eeoooue <pdmoorhouse@gmail.com>
2024-12-21 00:36:18 +0000

    Create DevelopmentConfig.cs

---
+++ Solution/MAli/AlignmentConfigs/DevelopmentConfig.cs
@@ -0,0 +1,39 @@
+﻿using LibAlignment;
+using LibAlignment.Aligners;
+using LibScoring.ObjectiveFunctions;
+using LibScoring.ScoringMatrices;
+using LibScoring;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAli.AlignmentConfigs
+{
+    internal class DevelopmentConfig : AlignmentConfig
+    {
+        public override Aligner CreateAligner()
+        {
+            return GetHCRandomRestarts();
+        }
+
+        public Aligner GetRandomSearchAligner()
+        {
+            IScoringMatrix blosum = new BLOSUM62Matrix();
+            IObjectiveFunction sumOfPairsWithAffine = new SumOfPairsWithAffineGapPenaltiesObjectiveFunction(blosum);
+            int iterations = 1000;
+
+            return new RandomSearchAligner(sumOfPairsWithAffine, iterations);
+        }
+
+        public Aligner GetHCRandomRestarts()
+        {
+            IScoringMatrix blosum = new BLOSUM62Matrix();
+            IObjectiveFunction sumOfPairsWithAffine = new SumOfPairsWithAffineGapPenaltiesObjectiveFunction(blosum);
+            int iterations = 1000;
+
+            return new HillClimbWithRandomRestartsAligner(sumOfPairsWithAffine, iterations);
+        }
+    }
+}
