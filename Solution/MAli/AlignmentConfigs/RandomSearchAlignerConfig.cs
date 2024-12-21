0aa8867fefdf6f7e18c76de9828eaea865a6e677
eeoooue <pdmoorhouse@gmail.com>
2024-12-21 00:14:19 +0000

    Create RandomSearchAlignerConfig.cs

---
+++ Solution/MAli/AlignmentConfigs/RandomSearchAlignerConfig.cs
@@ -0,0 +1,25 @@
+﻿using LibAlignment;
+using LibAlignment.Aligners;
+using LibScoring;
+using LibScoring.ObjectiveFunctions;
+using LibScoring.ScoringMatrices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAli.AlignmentConfigs
+{
+    public class RandomSearchAlignerConfig : AlignmentConfig
+    {
+        public override Aligner CreateAligner()
+        {
+            IScoringMatrix blosum = new BLOSUM62Matrix();
+            IObjectiveFunction sumOfPairsWithAffine = new SumOfPairsWithAffineGapPenaltiesObjectiveFunction(blosum);
+            int iterations = 1000;
+
+            return new RandomSearchAligner(sumOfPairsWithAffine, iterations);
+        }
+    }
+}
