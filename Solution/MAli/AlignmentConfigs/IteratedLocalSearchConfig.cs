ac29f1cb1d2f2540d987a62a71bcde9338026d7f
eeoooue <pdmoorhouse@gmail.com>
2025-01-10 06:25:39 +0000

    Create IteratedLocalSearchConfig.cs

---
+++ Solution/MAli/AlignmentConfigs/IteratedLocalSearchConfig.cs
@@ -0,0 +1,30 @@
+﻿using LibAlignment;
+using LibAlignment.Aligners;
+using LibScoring.ObjectiveFunctions;
+using LibScoring.ScoringMatrices;
+using LibScoring;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAli.AlignmentConfigs
+{
+    internal class IteratedLocalSearchConfig : AlignmentConfig
+    {
+        public override Aligner CreateAligner()
+        {
+            return GetILSAligner();
+        }
+
+        public Aligner GetILSAligner()
+        {
+            IScoringMatrix blosum = new BLOSUM62Matrix();
+            IObjectiveFunction sumOfPairsWithAffine = new SumOfPairsWithAffineGapPenaltiesObjectiveFunction(blosum);
+            int iterations = 10000;
+
+            return new IteratedLocalSearchAligner(sumOfPairsWithAffine, iterations);
+        }
+    }
+}
