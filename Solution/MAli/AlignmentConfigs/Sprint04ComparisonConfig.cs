7ff787aea7a5c1a5b2401d564e966c5afb61125b
eeoooue <pdmoorhouse@gmail.com>
2025-01-14 21:56:23 +0000

    scaffolded a Sprint04ComparisonConfig

---
+++ Solution/MAli/AlignmentConfigs/Sprint04ComparisonConfig.cs
@@ -0,0 +1,40 @@
+﻿using LibAlignment;
+using LibAlignment.Aligners;
+using LibAlignment.Aligners.PopulationBased;
+using LibBioInfo;
+using LibScoring;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAli.AlignmentConfigs
+{
+    internal class Sprint04ComparisonConfig : AlignmentConfig
+    {
+        public override IterativeAligner CreateAligner()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IAlignmentModifier GetOperator()
+        {
+            throw new NotImplementedException();
+        }
+        public IObjectiveFunction GetObjective()
+        {
+            throw new NotImplementedException();
+        }
+
+        public PopulationBasedAligner GetPopulationBasedAligner()
+        {
+            throw new NotImplementedException();
+        }
+
+        public SingleStateAligner GetSingleStateAligner()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
