b80f81a2b3a630cfba1a1eb39c49cace7b185d61
eeoooue <pdmoorhouse@gmail.com>
2025-01-21 17:38:55 +0000

    moved population-based methods into lower abstraction

--- Solution/LibAlignment/Aligners/PopulationBasedAligner.cs
+++ Solution/LibAlignment/Aligners/PopulationBasedAligner.cs
@@ -16,5 +16,64 @@ namespace LibAlignment.Aligners
         {
 
         }
+
+        public List<ScoredAlignment> ScorePopulation(List<Alignment> population)
+        {
+            List<ScoredAlignment> candidates = new List<ScoredAlignment>();
+            foreach (Alignment alignment in population)
+            {
+                double score = ScoreAlignment(alignment);
+                ScoredAlignment candidate = new ScoredAlignment(alignment, score);
+                candidates.Add(candidate);
+                CheckNewBest(candidate);
+            }
+
+            SetFitnesses(candidates);
+
+            return candidates;
+        }
+
+        public void SetFitnesses(List<ScoredAlignment> candidates)
+        {
+            double bestScore = GetBestScore(candidates);
+            double worstScore = GetWorstScore(candidates);
+            double range = bestScore - worstScore;
+
+            foreach (ScoredAlignment candidate in candidates)
+            {
+                SetFitness(candidate, worstScore, range);
+            }
+        }
+
+        public void SetFitness(ScoredAlignment candidate, in double worstScore, in double range)
+        {
+            double unscaledFitness = candidate.Score - worstScore;
+            double scaled = unscaledFitness / range;
+            candidate.Fitness = scaled;
+        }
+
+        public double GetBestScore(List<ScoredAlignment> candidates)
+        {
+            double bestScore = double.MinValue;
+            foreach (ScoredAlignment candidate in candidates)
+            {
+                double score = candidate.Score;
+                bestScore = Math.Max(score, bestScore);
+            }
+
+            return bestScore;
+        }
+
+        public double GetWorstScore(List<ScoredAlignment> candidates)
+        {
+            double worstScore = double.MaxValue;
+            foreach (ScoredAlignment candidate in candidates)
+            {
+                double score = candidate.Score;
+                worstScore = Math.Min(score, worstScore);
+            }
+
+            return worstScore;
+        }
     }
 }
