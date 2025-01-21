c13329bf11a1d425567cacaf09fb175ea169e3aa
eeoooue <pdmoorhouse@gmail.com>
2025-01-21 18:16:09 +0000

    created IIterativeAligner interface

---
+++ Solution/LibAlignment/IIterativeAligner.cs
@@ -0,0 +1,29 @@
+﻿using LibBioInfo;
+using LibScoring;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibAlignment
+{
+    public interface IIterativeAligner
+    {
+        public IObjectiveFunction Objective { get; }
+
+        public Alignment? CurrentAlignment { get; }
+
+        public int IterationsCompleted { get; } 
+
+        public int IterationsLimit { get; set; } 
+
+        public double AlignmentScore { get; }
+
+        public Alignment AlignSequences(List<BioSequence> sequences);
+
+        public void Initialize(List<BioSequence> sequences);
+
+        public void Iterate();
+    }
+}
