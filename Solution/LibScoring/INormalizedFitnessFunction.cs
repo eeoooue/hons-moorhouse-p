7842c386f1cdf472a66d792f3f87c22e12051598
eeoooue <pdmoorhouse@gmail.com>
2025-01-22 20:56:14 +0000

    scaffolded INormalizedFitnessFunction interface

---
+++ Solution/LibScoring/INormalizedFitnessFunction.cs
@@ -0,0 +1,20 @@
+﻿using LibBioInfo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibScoring
+{
+    internal interface INormalizedFitnessFunction
+    {
+        public double GetBestPossibleFitness(Alignment alignment);
+
+        public double GetWorstPossibleFitness(Alignment alignment);
+
+        public double GetFitness(Alignment alignment);
+
+        public string GetName();
+    }
+}
