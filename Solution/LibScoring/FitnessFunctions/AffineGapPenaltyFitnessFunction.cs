80e46ccae07a1217b25e25e5cc76b93593fd4a60
eeoooue <pdmoorhouse@gmail.com>
2025-01-22 22:24:58 +0000

    scaffolded AffineGapPenaltyFitnessFunction

---
+++ Solution/LibScoring/FitnessFunctions/AffineGapPenaltyFitnessFunction.cs
@@ -0,0 +1,40 @@
+﻿using LibScoring.Metrics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibScoring.FitnessFunctions
+{
+
+    public class AffineGapPenaltyFitnessFunction : NormalisedFitnessFunction
+    {
+        private AffineGapPenalties Penalties;
+
+        public AffineGapPenaltyFitnessFunction(double openingCost = 4, double nullCost = 1)
+        {
+            Penalties = new AffineGapPenalties(openingCost, nullCost);
+        }
+
+        public override double GetBestPossibleScore(char[,] alignment)
+        {
+            return 1.0 - Penalties.GetMinimumPossiblePenalty();
+        }
+
+        public override string GetName()
+        {
+            return $"Affine Gap Penalties (open={Penalties.OpeningCost}, null={Penalties.NullCost})";
+        }
+
+        public override double GetWorstPossibleScore(char[,] alignment)
+        {
+            return 1.0 - Penalties.GetMaximumPossiblePenalty(alignment);
+        }
+
+        public override double ScoreAlignment(char[,] alignment)
+        {
+            return 1.0 - Penalties.GetPenaltyForAlignmentMatrix(alignment);
+        }
+    }
+}
