7dfee16be6c8cfde511ecbf8de07b07707fcc7ec
eeoooue <pdmoorhouse@gmail.com>
2025-01-22 21:13:06 +0000

    simplified interface for fitness functions

--- Solution/LibScoring/IFitnessFunction.cs
+++ Solution/LibScoring/IFitnessFunction.cs
@@ -7,12 +7,8 @@ using System.Threading.Tasks;
 
 namespace LibScoring
 {
-    public interface INormalizedFitnessFunction
+    public interface IFitnessFunction
     {
-        public double GetBestPossibleFitness(char[,] alignment);
-
-        public double GetWorstPossibleFitness(char[,] alignment);
-
         public double GetFitness(char[,] alignment);
 
         public string GetName();
