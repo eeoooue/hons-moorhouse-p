877464723f2eafeba1ace2f6eb715f0400b28389
eeoooue <pdmoorhouse@gmail.com>
2025-01-22 22:44:50 +0000

    added tests for new objective functions: affine gaps, non-gaps, sum of pairs

---
+++ Solution/TestsUnitSuite/LibScoring/FitnessFunctions/NonGapsFitnessFunctionTests.cs
@@ -0,0 +1,47 @@
+﻿using LibBioInfo;
+using LibScoring.FitnessFunctions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestsHarness.Tools;
+using TestsHarness;
+using LibScoring;
+
+namespace TestsUnitSuite.LibScoring.FitnessFunctions
+{
+    [TestClass]
+    public class NonGapsFitnessFunctionTests
+    {
+        ExampleAlignments ExampleAlignments = Harness.ExampleAlignments;
+        NormalisedFitnessFunction FitnessFunction = new NonGapsFitnessFunction();
+
+        [TestMethod]
+        public void FitnessScoreIsNormalized()
+        {
+            Alignment alignment = ExampleAlignments.GetExampleA();
+            double score = FitnessFunction.GetFitness(alignment.GetCharacterMatrix());
+            Assert.IsTrue(0 <= score && score <= 1.0);
+        }
+
+        [TestMethod]
+        public void BestScoreIsGreaterThanWorstScore()
+        {
+            Alignment alignment = ExampleAlignments.GetExampleA();
+            double best = FitnessFunction.GetBestPossibleScore(alignment.GetCharacterMatrix());
+            double worst = FitnessFunction.GetWorstPossibleScore(alignment.GetCharacterMatrix());
+            Assert.IsTrue(best > worst);
+        }
+
+        [TestMethod]
+        public void RawScoreIsBetweenExtremes()
+        {
+            Alignment alignment = ExampleAlignments.GetExampleA();
+            double score = FitnessFunction.ScoreAlignment(alignment.GetCharacterMatrix());
+            double best = FitnessFunction.GetBestPossibleScore(alignment.GetCharacterMatrix());
+            double worst = FitnessFunction.GetWorstPossibleScore(alignment.GetCharacterMatrix());
+            Assert.IsTrue(worst <= score && score <= best);
+        }
+    }
+}
