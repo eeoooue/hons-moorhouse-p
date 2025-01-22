84725f38e5048875fa321cac8eeea557e4f6e536
eeoooue <pdmoorhouse@gmail.com>
2025-01-22 21:31:24 +0000

    scaffolded sum of pairs fitness function

---
+++ Solution/LibScoring/FitnessFunctions/SumOfPairsFitnessFunction.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibScoring.FitnessFunctions
+{
+    public class SumOfPairsFitnessFunction : NormalisedFitnessFunction
+    {
+        IScoringMatrix Matrix;
+
+        public SumOfPairsFitnessFunction(IScoringMatrix matrix)
+        {
+            Matrix = matrix;
+        }
+
+        public override string GetName()
+        {
+            return $"Sum of Pairs ({Matrix.GetName()})";
+        }
+
+        public override double ScoreAlignment(char[,] alignment)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetNumberOfPossiblePairs()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override double GetBestPossibleScore(char[,] alignment)
+        {
+            Matrix.GetBestPairwiseScorePossible();
+
+            throw new NotImplementedException();
+        }
+
+        public override double GetWorstPossibleScore(char[,] alignment)
+        {
+            Matrix.GetWorstPairwiseScorePossible();
+
+            throw new NotImplementedException();
+        }
+    }
+}
