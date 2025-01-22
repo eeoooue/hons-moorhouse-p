c6c80f1d85f5301b60dad5a0dd943b1357bff811
eeoooue <pdmoorhouse@gmail.com>
2025-01-22 21:56:13 +0000

    copied across sum of pairs implementation from existing objective

--- Solution/LibScoring/FitnessFunctions/SumOfPairsFitnessFunction.cs
+++ Solution/LibScoring/FitnessFunctions/SumOfPairsFitnessFunction.cs
@@ -22,7 +22,79 @@ namespace LibScoring.FitnessFunctions
 
         public override double ScoreAlignment(char[,] alignment)
         {
-            throw new NotImplementedException();
+            int n = alignment.GetLength(1);
+
+            double result = 0;
+            for (int j = 0; j < n; j++)
+            {
+                result += ScoreColumn(alignment, j);
+            }
+
+            return result;
+        }
+
+        public double ScoreColumn(char[,] alignment, int j)
+        {
+            Dictionary<char, int> table = ConstructCounterTableForColumn(alignment, j);
+            double result = ScorePairwiseCombinations(table);
+
+            return result;
+        }
+
+        public Dictionary<char, int> ConstructCounterTableForColumn(char[,] matrix, int j)
+        {
+            Dictionary<char, int> result = new Dictionary<char, int>();
+            foreach (char residue in Matrix.GetResidues())
+            {
+                result[residue] = 0;
+            }
+
+            int m = matrix.GetLength(0);
+            for (int i = 0; i < m; i++)
+            {
+                char x = matrix[i, j];
+                if (result.ContainsKey(x))
+                {
+                    result[x] += 1;
+                }
+            }
+
+            return result;
+        }
+
+        public double ScorePairwiseCombinations(Dictionary<char, int> table)
+        {
+            List<char> residues = Matrix.GetResidues();
+            double result = 0;
+
+            for (int i1 = 0; i1 < residues.Count; i1++)
+            {
+                char a = residues[i1];
+                int a_count = table[a];
+
+                for (int i2 = i1; i2 < residues.Count; i2++)
+                {
+                    char b = residues[i2];
+                    int b_count = table[b];
+
+                    int combinations = 0;
+
+                    if (a != b)
+                    {
+                        combinations = (a_count * b_count) / 2;
+                    }
+                    else
+                    {
+                        combinations = (a_count * (a_count - 1)) / 2;
+                    }
+
+                    int score = Matrix.ScorePair(a, b);
+                    int contribution = score * combinations;
+                    result += contribution;
+                }
+            }
+
+            return result;
         }
 
         public int GetNumberOfPossiblePairs(char[,] alignment)
