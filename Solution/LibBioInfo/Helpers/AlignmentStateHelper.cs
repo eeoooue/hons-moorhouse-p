4cc7562d0228fe7577ef1028402f54242e4636e7
eeoooue <pdmoorhouse@gmail.com>
2025-01-23 01:28:36 +0000

    added new alignment state helper methods

--- Solution/LibBioInfo/Helpers/AlignmentStateHelper.cs
+++ Solution/LibBioInfo/Helpers/AlignmentStateHelper.cs
@@ -8,6 +8,55 @@ namespace LibBioInfo.Helpers
 {
     public class AlignmentStateHelper
     {
+        private Bioinformatics Bioinformatics = new Bioinformatics();
+
+        public bool[,] ConvertMatrixFromCharToBool(char[,] state)
+        {
+            int m = state.GetLength(0);
+            int n = state.GetLength(1);
+
+            bool[,] result = new bool[m, n];
+
+            for(int i=0; i<m; i++)
+            {
+                for(int j=0; j<n; j++)
+                {
+                    result[i, j] = false;
+                    if (Bioinformatics.IsGapChar(state[i, j]))
+                    {
+                        result[i, j] = true;
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        public char[,] ConvertMatrixFromBoolToChar(List<BioSequence> sequences, bool[,] state)
+        {
+            int m = state.GetLength(0);
+            int n = state.GetLength(1);
+
+            char[,] result = new char[m, n];
+
+            for(int i=0; i<m; i++)
+            {
+                string residues = sequences[i].Residues;
+                int p = 0;
+                for(int j=0; j<n; j++)
+                {
+                    result[i, j] = '-';
+                    if (!state[i, j])
+                    {
+                        char x = residues[p++];
+                        result[i, j] = x;
+                    }
+                }
+            }
+
+            return result;
+        }
+
         public bool[,] RemoveEmptyColumns(bool[,] state)
         {
             List<int> targets = CollectNonEmptyColumnIndices(state);
