152896ab6333d7bea725ef2981743683ff7fd5dc
eeoooue <pdmoorhouse@gmail.com>
2025-01-27 02:14:41 +0000

    scaffolded a PairwiseAligner interface

---
+++ Solution/LibBioInfo/IPairwiseAligner.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibBioInfo
+{
+    public interface IPairwiseAligner
+    {
+        public char[,] AlignSequences(string sequenceA, string sequenceB);
+    }
+}
---
+++ Solution/LibBioInfo/PairwiseAligners/NeedlemanWunschAligner.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibBioInfo.PairwiseAligners
+{
+    public class NeedlemanWunschAligner : IPairwiseAligner
+    {
+
+
+
+        public char[,] AlignSequences(string sequenceA, string sequenceB)
+        {
+            int m = sequenceA.Length;
+            int n = sequenceB.Length;
+
+            char[,] result = new char[m, n];
+
+            return result;
+        }
+
+
+
+
+
+
+    }
+}
