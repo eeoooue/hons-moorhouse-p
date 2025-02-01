18238779b0ba75ed7e1c5869114e31a933f047e0
eeoooue <pdmoorhouse@gmail.com>
2025-02-01 09:17:25 +0000

    introduced AlignmentHelper & ArgumentHelper to simplify MAli interface & facade

---
+++ Solution/MAli/Helpers/AlignmentHelper.cs
@@ -0,0 +1,135 @@
+﻿using LibAlignment;
+using LibBioInfo;
+using LibFileIO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAli.Helpers
+{
+    internal class AlignmentHelper
+    {
+        private FileHelper FileHelper = new FileHelper();
+        private FrameHelper FrameHelper = new FrameHelper();
+        private ResponseBank ResponseBank = new ResponseBank();
+
+        private ArgumentHelper ArgumentHelper = new ArgumentHelper();
+
+        private AlignmentConfig Config;
+
+        public AlignmentHelper(AlignmentConfig config)
+        {
+            Config = config;
+        }
+
+        public void PerformAlignment(string inputPath, string outputPath, Dictionary<string, string?> table)
+        {
+            bool debugging = ArgumentHelper.CommandsIncludeFlag(table, "debug");
+            bool emitFrames = ArgumentHelper.CommandsIncludeFlag(table, "frames");
+            bool refineOnly = ArgumentHelper.CommandsIncludeFlag(table, "refine");
+
+            try
+            {
+                Console.WriteLine($"Reading sequences from source: '{inputPath}'");
+                List<BioSequence> sequences = FileHelper.ReadSequencesFrom(inputPath);
+                Alignment alignment = new Alignment(sequences, true);
+
+                if (alignment.SequencesCanBeAligned())
+                {
+                    IIterativeAligner aligner = Config.CreateAligner();
+
+                    if (debugging && aligner is IterativeAligner instance)
+                    {
+                        aligner = new DebuggingWrapper(instance);
+                    }
+
+                    int iterations = ArgumentHelper.UnpackSpecifiedIterations(table);
+                    if (iterations > 0)
+                    {
+                        aligner.IterationsLimit = iterations;
+                    }
+
+                    if (refineOnly)
+                    {
+                        aligner.InitializeForRefinement(alignment);
+                    }
+                    else
+                    {
+                        aligner.Initialize(alignment.Sequences);
+                    }
+
+                    AlignIteratively(aligner, emitFrames, refineOnly);
+
+                    string outputFilename = BuildFullOutputFilename(outputPath, table);
+
+                    FileHelper.WriteAlignmentTo(aligner.CurrentAlignment!, outputFilename);
+                    Console.WriteLine($"Alignment written to destination: '{outputFilename}'");
+                }
+                else
+                {
+                    Console.WriteLine("Error: Sequences cannot be aligned.");
+                }
+            }
+            catch (Exception e)
+            {
+                ResponseBank.ExplainException(e);
+            }
+        }
+
+        public void AlignIteratively(IIterativeAligner aligner, bool emitFrames, bool refineOnly)
+        {
+            string context = $"Performing Multiple Sequence Alignment: {aligner.IterationsLimit} iterations.";
+            if (refineOnly)
+            {
+                context += " (iterative refinement)";
+            }
+
+            Console.WriteLine(context);
+
+            if (emitFrames)
+            {
+                FrameHelper.CheckCreateFramesFolder();
+            }
+
+            while (aligner.IterationsCompleted < aligner.IterationsLimit)
+            {
+                aligner.Iterate();
+                if (emitFrames && aligner.CurrentAlignment is Alignment alignment)
+                {
+                    FrameHelper.SaveCurrentFrame(alignment, aligner.IterationsCompleted);
+                }
+            }
+        }
+
+        
+
+
+        public string BuildFullOutputFilename(string outputName, Dictionary<string, string?> table)
+        {
+            string result = outputName;
+            if (ArgumentHelper.CommandsIncludeFlag(table, "timestamp"))
+            {
+                result += $"_{GetTimeStamp()}";
+            }
+            if (ArgumentHelper.CommandsIncludeFlag(table, "tag"))
+            {
+                string? specifiedTag = table["tag"];
+                if (specifiedTag is string tag)
+                {
+                    result += $"_{tag}";
+                }
+            }
+            result += ".faa";
+
+            return result;
+        }
+
+        public string GetTimeStamp()
+        {
+            string timestamp = DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss");
+            return timestamp;
+        }
+    }
+}
--- Solution/MAli/MAliFacade.cs
+++ Solution/MAli/MAliFacade.cs
@@ -15,160 +15,24 @@ namespace MAli
 {
     public class MAliFacade
     {
-        private FileHelper FileHelper = new FileHelper();
-
-        private FrameHelper FrameHelper = new FrameHelper();
+        public static AlignmentConfig Config = new Sprint05Config();
 
         private ResponseBank ResponseBank = new ResponseBank();
-        public AlignmentConfig Config = new Sprint05Config();
+
+        private AlignmentHelper AlignmentHelper = new AlignmentHelper(Config);
+
 
         public void SetSeed(string value)
         {
-            try
+            if (int.TryParse(value, out int seed))
             {
-                int seedValue = int.Parse(value);
-                Randomizer.SetSeed(seedValue);
-            }
-            catch
-            {
-                return;
+                Randomizer.SetSeed(seed);
             }
         }
 
         public void PerformAlignment(string inputPath, string outputPath, Dictionary<string, string?> table)
         {
-            bool debugging = CommandsIncludeFlag(table, "debug");
-            bool emitFrames = CommandsIncludeFlag(table, "frames");
-            bool refineOnly = CommandsIncludeFlag(table, "refine");
-
-            try
-            {
-                Console.WriteLine($"Reading sequences from source: '{inputPath}'");
-                List<BioSequence> sequences = FileHelper.ReadSequencesFrom(inputPath);
-                Alignment alignment = new Alignment(sequences, true);
-
-                if (alignment.SequencesCanBeAligned())
-                {
-                    IIterativeAligner aligner = Config.CreateAligner();
-
-                    if (debugging && aligner is IterativeAligner instance)
-                    {
-                        aligner = new DebuggingWrapper(instance);
-                    }
-
-                    int iterations = UnpackSpecifiedIterations(table);
-                    if (iterations > 0)
-                    {
-                        aligner.IterationsLimit = iterations;
-                    }
-
-                    if (refineOnly)
-                    {
-                        aligner.InitializeForRefinement(alignment);
-                    }
-                    else
-                    {
-                        aligner.Initialize(alignment.Sequences);
-                    }
-
-                    AlignIteratively(aligner, emitFrames, refineOnly);
-
-                    string outputFilename = BuildFullOutputFilename(outputPath, table);
-
-                    FileHelper.WriteAlignmentTo(aligner.CurrentAlignment!, outputFilename);
-                    Console.WriteLine($"Alignment written to destination: '{outputFilename}'");
-                }
-                else
-                {
-                    Console.WriteLine("Error: Sequences cannot be aligned.");
-                }
-            }
-            catch (Exception e)
-            {
-                ResponseBank.ExplainException(e);
-            }
-        }
-
-        public bool CommandsIncludeFlag(Dictionary<string, string?> table, string flag)
-        {
-            bool result = table.ContainsKey(flag);
-            return result;
-        }
-
-        public void AlignIteratively(IIterativeAligner aligner, bool emitFrames, bool refineOnly)
-        {
-            string context = $"Performing Multiple Sequence Alignment: {aligner.IterationsLimit} iterations.";
-            if (refineOnly)
-            {
-                context += " (iterative refinement)";
-            }
-
-            Console.WriteLine(context);
-
-            if (emitFrames)
-            {
-                FrameHelper.CheckCreateFramesFolder();
-            }
-
-            while(aligner.IterationsCompleted < aligner.IterationsLimit)
-            {
-                aligner.Iterate();
-                if (emitFrames && aligner.CurrentAlignment is Alignment alignment)
-                {
-                    FrameHelper.SaveCurrentFrame(alignment, aligner.IterationsCompleted);
-                }
-            }
-        }
-
-        
-
-        public int UnpackSpecifiedIterations(Dictionary<string, string?> table)
-        {
-            if (table.ContainsKey("iterations"))
-            {
-                string? iterationsValue = table["iterations"];
-
-                if (iterationsValue is string iterations)
-                {
-                    int result = 0;
-                    if (int.TryParse(iterations, out result))
-                    {
-                        return result;
-                    }
-                }
-            }
-
-            return 0;
-        }
-
-
-        public string BuildFullOutputFilename(string outputName, Dictionary<string, string?> table)
-        {
-            string result = outputName;
-
-            if (CommandsIncludeFlag(table, "timestamp"))
-            {
-                result += $"_{GetTimeStamp()}";
-            }
-
-            if (CommandsIncludeFlag(table, "tag"))
-            {
-                string? specifiedTag = table["tag"];
-                if (specifiedTag is string tag)
-                {
-                    result += $"_{tag}";
-                }
-            }
-
-            result += ".faa";
-
-            return result;
-        }
-
-        public string GetTimeStamp()
-        {
-            string timestamp = DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss");
-            return timestamp;
+            AlignmentHelper.PerformAlignment(inputPath, outputPath, table);
         }
 
         public void ProvideHelp()
