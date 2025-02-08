0915878cb6b34380ae2edce8ff71a96a19b8d00a
eeoooue <pdmoorhouse@gmail.com>
2025-02-08 10:20:55 +0000

    scaffolded a BatchAlignmentHelper class

---
+++ Solution/MAli/Helpers/BatchAlignmentHelper.cs
@@ -0,0 +1,32 @@
+﻿using LibAlignment;
+using LibBioInfo;
+using LibFileIO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAli.Helpers
+{
+    internal class BatchAlignmentHelper
+    {
+        private FileHelper FileHelper = new FileHelper();
+        private FrameHelper FrameHelper = new FrameHelper();
+        private ResponseBank ResponseBank = new ResponseBank();
+        private ArgumentHelper ArgumentHelper = new ArgumentHelper();
+        private AlignmentConfig Config;
+        private AlignmentHelper AlignmentHelper;
+
+        public BatchAlignmentHelper(AlignmentConfig config)
+        {
+            Config = config;
+            AlignmentHelper = new AlignmentHelper(config);
+        }
+
+        public void PerformBatchAlignment(string inDirectory, string outDirectory, Dictionary<string, string?> table)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
