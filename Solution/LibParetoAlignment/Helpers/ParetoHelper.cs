23c34d6b8f1a33cacb12112f102eedbe620aba93
eeoooue <pdmoorhouse@gmail.com>
2025-02-09 11:36:14 +0000

    scaffolded a pareto helper for pareto dominance logic

---
+++ Solution/LibParetoAlignment/Helpers/ParetoHelper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibParetoAlignment.Helpers
+{
+    internal class ParetoHelper
+    {
+        public bool ADominatesB(TradeoffAlignment a, TradeoffAlignment b, List<string> objectives)
+        {
+            bool includesImprovement = false;
+
+            foreach(string objective in objectives)
+            {
+                double aScore = a.Scores[objective];
+                double bScore = b.Scores[objective];
+
+                if (aScore > bScore)
+                {
+                    includesImprovement = true;
+                }
+                else if (aScore < bScore)
+                {
+                    return false;
+                }
+            }
+
+            return includesImprovement;
+        }
+
+        public bool ADominatesB(TradeoffAlignment a, TradeoffAlignment b)
+        {
+            List<string> objectives = a.Scores.Keys.ToList();
+            return ADominatesB(a, b, objectives);
+        }
+    }
+}
