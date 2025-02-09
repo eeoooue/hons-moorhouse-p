3ba23d5eeaa2e4d6a6eb92439176d11eb98421f6
eeoooue <pdmoorhouse@gmail.com>
2025-02-09 13:29:52 +0000

    scaffolded pareto alignment config

---
+++ Solution/MAli/ParetoAlignmentConfig.cs
@@ -0,0 +1,41 @@
+﻿using LibAlignment;
+using LibBioInfo;
+using LibParetoAlignment;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAli
+{
+    public abstract class ParetoAlignmentConfig
+    {
+        public abstract ParetoIterativeAligner CreateAligner();
+
+        public ParetoIterativeAligner InitialiseAligner(Alignment alignment, AlignmentInstructions instructions)
+        {
+            ParetoIterativeAligner aligner = CreateAligner();
+
+            if (instructions.IterationsLimit > 0)
+            {
+                aligner.IterationsLimit = instructions.IterationsLimit;
+            }
+            else
+            {
+                aligner.IterationsLimit = instructions.IterationsLimit;
+            }
+
+            if (instructions.RefineOnly)
+            {
+                aligner.InitializeForRefinement(alignment);
+            }
+            else
+            {
+                aligner.Initialize(alignment.Sequences);
+            }
+
+            return aligner;
+        }
+    }
+}
