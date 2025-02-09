705568eb26a925842dd06280000f6cd89605586a
eeoooue <pdmoorhouse@gmail.com>
2025-02-09 13:22:44 +0000

    scaffolded new classes for pareto alignment - helper + configs

---
+++ Solution/MAli/Helpers/ParetoAlignmentHelper.cs
@@ -0,0 +1,28 @@
+﻿using LibFileIO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAli.Helpers
+{
+    public class ParetoAlignmentHelper
+    {
+        private FileHelper FileHelper = new FileHelper();
+        private ResponseBank ResponseBank = new ResponseBank();
+        private ArgumentHelper ArgumentHelper = new ArgumentHelper();
+        // private AlignmentConfig Config;
+        private DebuggingHelper DebuggingHelper = new DebuggingHelper();
+
+
+
+
+
+
+
+
+
+
+    }
+}
