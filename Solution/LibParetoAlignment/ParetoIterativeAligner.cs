8415554e4cb535c3dda473246ea9e85c6b391937
eeoooue <pdmoorhouse@gmail.com>
2025-02-09 11:53:54 +0000

    scaffolded new ParetoIterativeAligner abstraction

---
+++ Solution/LibParetoAlignment/ParetoIterativeAligner.cs
@@ -0,0 +1,36 @@
+﻿using LibBioInfo;
+using LibScoring;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibParetoAlignment
+{
+    internal class ParetoIterativeAligner
+    {
+        List<IFitnessFunction> Objectives;
+
+        public ParetoIterativeAligner(List<IFitnessFunction> objectives)
+        {
+            Objectives = objectives;
+        }
+
+
+
+
+        public TradeoffAlignment EvaluateAlignment(Alignment alignment)
+        {
+            TradeoffAlignment result = new TradeoffAlignment(alignment);
+            foreach(IFitnessFunction objective in Objectives)
+            {
+                string name = objective.GetName();
+                double score = objective.GetFitness(alignment.CharacterMatrix);
+                result.SetScore(name, score);
+            }
+
+            return result;
+        }
+    }
+}
