fe6361e466c3ddd9d02a7d49689357190b53dc8c
eeoooue <pdmoorhouse@gmail.com>
2025-02-10 20:14:33 +0000

    moved json element parsing into helper class

--- Solution/MAli/AlignmentConfigs/UserConfig.cs
+++ Solution/MAli/AlignmentConfigs/UserConfig.cs
@@ -34,68 +34,9 @@ namespace MAli.AlignmentConfigs
             IterativeAligner aligner = BaseConfig.CreateAligner();
             JsonElement root = Helper.ReadConfigFrom("config.json");
             JsonElement objElement = root.GetProperty("Objective");
-            aligner.Objective = ExtractObjective(objElement);
+            aligner.Objective = Helper.ExtractObjective(objElement);
 
             return aligner;
-        }
-
-        public IFitnessFunction ExtractObjective(JsonElement element)
-        {
-            string name = element.GetProperty("Name").GetString()!;
-
-            if (name == "WeightedCombination")
-            {
-                List<IFitnessFunction> objectives = new List<IFitnessFunction>();
-                JsonElement objElement = element.GetProperty("Objectives");
-                foreach(JsonElement objChild in objElement.EnumerateArray())
-                {
-                    IFitnessFunction objective = ExtractObjective(objChild);
-                    objectives.Add(objective);
-
-                }
-
-                List<double> weights = new List<double>();
-                JsonElement weightsElement = element.GetProperty("Weights");
-                foreach(JsonElement weightChild in weightsElement.EnumerateArray())
-                {
-                    weights.Add(weightsElement.GetDouble());
-                }
-
-                return new WeightedCombinationOfFitnessFunctions(objectives, weights);
-            }
-
-            if (name == "SumOfPairs")
-            {
-                string matrixValue = element.GetProperty("Matrix").GetString()!;
-                IScoringMatrix matrix = GetMatchingMatrix(matrixValue);
-                return new SumOfPairsFitnessFunction(matrix);
-            }
-
-            if (name == "PercentTCCs")
-            {
-                return new TotallyConservedColumnsFitnessFunction();
-            }
-
-            if (name == "PercentNonGaps")
-            {
-                return new NonGapsFitnessFunction();
-            }
-
-            throw new KeyNotFoundException("Failed to find matching objective function.");
-        }
-
-
-        private IScoringMatrix GetMatchingMatrix(string value)
-        {
-            switch (value)
-            {
-                case "BLOSUM62":
-                    return new BLOSUM62Matrix();
-                case "PAM250":
-                    return new PAM250Matrix();
-                default:
-                    return new IdentityMatrix();
-            }
         }
     }
 }
--- Solution/MAli/Helpers/JsonConfigHelper.cs
+++ Solution/MAli/Helpers/JsonConfigHelper.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using LibBioInfo.ScoringMatrices;
+using LibBioInfo;
+using LibScoring.FitnessFunctions;
+using LibScoring;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +19,64 @@ namespace MAli.Helpers
             JsonDocument document = JsonDocument.Parse(text);
 
             return document.RootElement;
+        }
+
+        public IFitnessFunction ExtractObjective(JsonElement element)
+        {
+            string name = element.GetProperty("Name").GetString()!;
+
+            if (name == "WeightedCombination")
+            {
+                List<IFitnessFunction> objectives = new List<IFitnessFunction>();
+                JsonElement objElement = element.GetProperty("Objectives");
+                foreach (JsonElement objChild in objElement.EnumerateArray())
+                {
+                    IFitnessFunction objective = ExtractObjective(objChild);
+                    objectives.Add(objective);
+
+                }
+
+                List<double> weights = new List<double>();
+                JsonElement weightsElement = element.GetProperty("Weights");
+                foreach (JsonElement weightChild in weightsElement.EnumerateArray())
+                {
+                    weights.Add(weightsElement.GetDouble());
+                }
+
+                return new WeightedCombinationOfFitnessFunctions(objectives, weights);
+            }
+
+            if (name == "SumOfPairs")
+            {
+                string matrixValue = element.GetProperty("Matrix").GetString()!;
+                IScoringMatrix matrix = GetMatchingMatrix(matrixValue);
+                return new SumOfPairsFitnessFunction(matrix);
+            }
+
+            if (name == "PercentTCCs")
+            {
+                return new TotallyConservedColumnsFitnessFunction();
+            }
+
+            if (name == "PercentNonGaps")
+            {
+                return new NonGapsFitnessFunction();
+            }
+
+            throw new KeyNotFoundException("Failed to find matching objective function.");
+        }
+
+        private IScoringMatrix GetMatchingMatrix(string value)
+        {
+            switch (value)
+            {
+                case "BLOSUM62":
+                    return new BLOSUM62Matrix();
+                case "PAM250":
+                    return new PAM250Matrix();
+                default:
+                    return new IdentityMatrix();
+            }
         }
     }
 }
