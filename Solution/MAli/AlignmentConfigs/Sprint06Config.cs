f0ba5324f02dba0175039445e17831b680289d6d
eeoooue <pdmoorhouse@gmail.com>
2025-02-13 17:01:38 +0000

    created new Sprint06Config

---
+++ Solution/MAli/AlignmentConfigs/Sprint06Config.cs
@@ -0,0 +1,56 @@
+﻿using LibAlignment.Aligners.PopulationBased;
+using LibAlignment.Aligners.SingleState;
+using LibAlignment;
+using LibBioInfo.ScoringMatrices;
+using LibBioInfo;
+using LibModification.AlignmentModifiers;
+using LibModification.CrossoverOperators;
+using LibModification;
+using LibScoring.FitnessFunctions;
+using LibScoring;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAli.AlignmentConfigs
+{
+    public class Sprint06Config : AlignmentConfig
+    {
+        public override IterativeAligner CreateAligner()
+        {
+            return GetSprint05Config();
+        }
+
+        public IFitnessFunction GetObjective()
+        {
+            IScoringMatrix matrix = new BLOSUM62Matrix();
+            IFitnessFunction objective = new SumOfPairsWithAffineGapPenaltiesFitnessFunction(matrix, 4, 1);
+
+            return objective;
+        }
+
+        private IterativeAligner GetSprint05Config()
+        {
+            IFitnessFunction objective = GetObjective();
+
+            const int maxIterations = 100;
+
+            IteratedLocalSearchAligner aligner = new IteratedLocalSearchAligner(objective, maxIterations);
+
+            List<IAlignmentModifier> modifiers = new List<IAlignmentModifier>()
+            {
+                new SwapOperator(),
+                new GapInserter(),
+                new MultiRowStochasticSwapOperator(),
+                new HeuristicPairwiseModifier(),
+            };
+
+            aligner.TweakModifier = new MultiOperatorModifier(modifiers);
+            aligner.PerturbModifier = new MultiRowStochasticSwapOperator();
+
+            return aligner;
+        }
+    }
+}
