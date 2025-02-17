38e59bc4ea084b88ecc98a740e10ccf2c3966252
eeoooue <pdmoorhouse@gmail.com>
2025-02-17 19:52:12 +0000

    adopted flyweight pattern for Alignment abstraction instances

--- Solution/LibBioInfo/Alignment.cs
+++ Solution/LibBioInfo/Alignment.cs
@@ -5,19 +5,19 @@ namespace LibBioInfo
 {
     public class Alignment
     {
-        public List<BioSequence> Sequences;
+        public List<BioSequence> Sequences { get { return AlignmentCore.Sequences; } }
         public int Height { get { return CharacterMatrix.GetLength(0); } }
         public int Width { get { return CharacterMatrix.GetLength(1); } }
-
-        private static Bioinformatics Bioinformatics = new Bioinformatics();
 
         public char[,] CharacterMatrix;
 
         public int[] ProgressiveOrder;
 
+        public AlignmentCore AlignmentCore;
+
         public Alignment(List<BioSequence> sequences, bool conserveState=false)
         {
-            Sequences = sequences;
+            AlignmentCore = new AlignmentCore(sequences);
             if (conserveState)
             {
                 CharacterMatrix = ConstructConservedAlignmentState(sequences);
@@ -36,8 +36,8 @@ namespace LibBioInfo
 
         public Alignment(Alignment other)
         {
-            Sequences = other.GetAlignedSequences();
-            CharacterMatrix = ConstructConservedAlignmentState(Sequences);
+            AlignmentCore = other.AlignmentCore;
+            CharacterMatrix = CopyCharacterMatrix(other.CharacterMatrix);
 
             ProgressiveOrder = new int[Height];
             for (int i = 0; i < Height; i++)
@@ -47,10 +47,28 @@ namespace LibBioInfo
         }
 
 
+        public char[,] CopyCharacterMatrix(char[,] matrix)
+        {
+            int m = matrix.GetLength(0);
+            int n = matrix.GetLength(1);
+
+            char[,] result = new char[m, n];
+            for(int i=0; i<m; i++)
+            {
+                for(int j=0; j<n; j++)
+                {
+                    result[i, j] = matrix[i, j];
+                }
+            }
+
+            return result;
+        }
+
+
         public char[,] ConstructConservedAlignmentState(List<BioSequence> sequences)
         {
             int m = sequences.Count;
-            int n = GetLongestPayloadLength(sequences);
+            int n = AlignmentCore.GetLongestPayloadLength(sequences);
 
             List<string> payloads = new List<string>();
             foreach(BioSequence sequence in sequences)
@@ -97,17 +115,6 @@ namespace LibBioInfo
 
             return result;
         }
-
-        public int GetLongestPayloadLength(List<BioSequence> sequences)
-        {
-            int result = 0;
-            foreach(BioSequence sequence in sequences)
-            {
-                result = Math.Max(sequence.Payload.Length, result);
-            }
-
-            return result;
-        }
         
         public List<BioSequence> GetAlignedSequences()
         {
@@ -127,24 +134,6 @@ namespace LibBioInfo
         public Alignment GetCopy()
         {
             return new Alignment(this);
-        }
-
-
-        public char GetCharacterAt(int i, int j)
-        {
-            return CharacterMatrix[i, j];
-        }
-
-        public string GetColumn(int j)
-        {
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < Height; i++)
-            {
-                char c = CharacterMatrix[i, j];
-                sb.Append(c);
-            }
-
-            return sb.ToString();
         }
 
         public string GetAlignedPayload(int i)
@@ -176,58 +165,12 @@ namespace LibBioInfo
 
         public bool ContainsNucleicsOnly()
         {
-            foreach (BioSequence sequence in Sequences)
-            {
-                if (!sequence.IsNucleic())
-                {
-                    return false;
-                }
-            }
-
-            return true;
+            return AlignmentCore.ContainsNucleicsOnly();
         }
 
         public bool ContainsProteinsOnly()
         {
-            foreach (BioSequence sequence in Sequences)
-            {
-                if (!sequence.IsProtein())
-                {
-                    return false;
-                }
-            }
-
-            return true;
-        }
-
-        public List<int> GetResiduePositionsInRow(Alignment alignment, int i)
-        {
-            List<int> result = new List<int>();
-
-            for(int j=0; j<alignment.Width; j++)
-            {
-                if (Bioinformatics.IsGapChar(CharacterMatrix[i,j]) == false)
-                {
-                    result.Add(j);
-                }
-            }
-
-            return result;
-        }
-
-        public List<int> GetGapPositionsInRow(Alignment alignment, int i)
-        {
-            List<int> result = new List<int>();
-
-            for (int j = 0; j < alignment.Width; j++)
-            {
-                if (Bioinformatics.IsGapChar(CharacterMatrix[i, j]) == true)
-                {
-                    result.Add(j);
-                }
-            }
-
-            return result;
+            return AlignmentCore.ContainsProteinsOnly();
         }
 
         public bool SequencesCanBeAligned()
