33f220e9581990fa617a5a2541a58b2068eb52bc
eeoooue <pdmoorhouse@gmail.com>
2025-02-17 19:28:51 +0000

    ProgressiveOrderModifier makes use of HeuristicPairwiseModifier

--- Solution/LibModification/AlignmentModifiers/ProgressiveOrderModifier.cs
+++ Solution/LibModification/AlignmentModifiers/ProgressiveOrderModifier.cs
@@ -1,4 +1,6 @@
 ﻿using LibBioInfo;
+using LibBioInfo.PairwiseAligners;
+using LibModification.Helpers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,12 +9,12 @@ using System.Threading.Tasks;
 
 namespace LibModification.AlignmentModifiers
 {
-    public class ProgressiveOrderModifier : IAlignmentModifier
+    public class ProgressiveOrderModifier : AlignmentModifier
     {
-        public void ModifyAlignment(Alignment alignment)
-        {
-            SwapRandomIndicesWithinArray(ref alignment.ProgressiveOrder);
-        }
+        CharMatrixHelper CharMatrixHelper = new CharMatrixHelper();
+        BiosequencePayloadHelper PayloadHelper = new BiosequencePayloadHelper();
+        Bioinformatics Bioinformatics = new Bioinformatics();
+        HeuristicPairwiseModifier PairwiseModifier = new HeuristicPairwiseModifier();
 
         public void SwapRandomIndicesWithinArray(ref int[] arr)
         {
@@ -33,6 +35,44 @@ namespace LibModification.AlignmentModifiers
             int y = arr[j];
             arr[i] = y;
             arr[j] = x;
+        }
+
+        public override char[,] GetModifiedAlignmentState(Alignment alignment)
+        {
+            int i;
+            int j;
+            PickRandomPairOfSequences(alignment, out i, out j);
+            SwapIndicesWithinArray(ref alignment.ProgressiveOrder, i, j);
+
+            if (Randomizer.CoinFlip())
+            {
+                return AlignNeighbourOf(alignment, i);
+            }
+            else
+            {
+                return AlignNeighbourOf(alignment, j);
+            }
+        }
+
+        public char[,] AlignNeighbourOf(Alignment alignment, int i)
+        {
+            int delta = Randomizer.CoinFlip() ? -1 : 1;
+            int j = (i + delta + alignment.Height) % alignment.Height;
+
+            int seqAindex = alignment.ProgressiveOrder[i];
+            int seqBindex = alignment.ProgressiveOrder[j];
+
+            return PairwiseModifier.AlignPairOfSequences(alignment, seqAindex, seqBindex);
+        }
+
+        public void PickRandomPairOfSequences(Alignment alignment, out int i, out int j)
+        {
+            i = Randomizer.Random.Next(alignment.Height);
+            j = i;
+            while (i == j)
+            {
+                j = Randomizer.Random.Next(alignment.Height);
+            }
         }
     }
 }
