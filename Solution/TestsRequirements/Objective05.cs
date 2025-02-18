7083d76c5f66b220103f3c8c264caead37610372
eeoooue <pdmoorhouse@gmail.com>
2025-02-18 20:48:41 +0000

    revised objective 5 tests

--- Solution/TestsRequirements/Objective05.cs
+++ Solution/TestsRequirements/Objective05.cs
@@ -1,21 +1,33 @@
-﻿using System;
+﻿using LibBioInfo;
+using LibBioInfo.ScoringMatrices;
+using LibParetoAlignment;
+using LibParetoAlignment.Aligners;
+using LibScoring;
+using LibScoring.FitnessFunctions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TestsHarness;
+using TestsHarness.Tools;
 
 namespace TestsRequirements
 {
     [TestClass]
     public class Objective05
     {
+        ExampleAlignments ExampleAlignments = Harness.ExampleAlignments;
+
         /// <summary>
         /// Leverages multiple objective functions to guide the alignment optimization process.
         /// </summary>
         [TestMethod]
         public void Req5x01()
         {
-            throw new NotImplementedException();
+            ParetoIterativeAligner aligner = GetMultiObjectiveAligner();
+            InitializeAlignerWithSequences(aligner);
+            PerformIterations(aligner, 100);
         }
 
         /// <summary>
@@ -24,7 +36,43 @@ namespace TestsRequirements
         [TestMethod]
         public void Req5x02()
         {
-            throw new NotImplementedException();
+            ParetoIterativeAligner aligner = GetMultiObjectiveAligner();
+            InitializeAlignerWithSequences(aligner);
+            PerformIterations(aligner, 100);
+
+            List<Alignment> tradeoffs = aligner.CollectTradeoffSolutions();
+            Assert.IsTrue(tradeoffs.Count > 1);
+        }
+
+
+        private void InitializeAlignerWithSequences(ParetoIterativeAligner aligner)
+        {
+            Alignment alignment = ExampleAlignments.GetExampleA();
+            List<BioSequence> sequences = alignment.Sequences;
+            aligner.Initialize(sequences);
+        }
+
+        private void PerformIterations(ParetoIterativeAligner aligner, int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                aligner.Iterate();
+            }
+        }
+
+        private ParetoIterativeAligner GetMultiObjectiveAligner()
+        {
+            IScoringMatrix matrix = new BLOSUM62Matrix();
+
+            List<IFitnessFunction> objectives = new List<IFitnessFunction>()
+            {
+                new SumOfPairsFitnessFunction(matrix),
+                new TotallyConservedColumnsFitnessFunction(),
+                new NonGapsFitnessFunction(),
+            };
+
+            ParetoIterativeAligner aligner = new ParetoHillClimbAligner(objectives);
+            return aligner;
         }
     }
 }
