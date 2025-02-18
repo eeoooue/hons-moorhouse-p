17f1682d2bca11dc6d52c060a08870f1ad94f717
eeoooue <pdmoorhouse@gmail.com>
2025-02-18 19:33:04 +0000

    Create Objective02.cs

---
+++ Solution/TestsRequirements/Objective02.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestsRequirements
+{
+    [TestClass]
+    public class Objective02
+    {
+        [TestMethod]
+        public void Req2x01()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        [TestMethod]
+        public void Req2x02()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
