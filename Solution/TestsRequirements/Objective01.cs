f2f5af7cfefaa2b4a7cbae3040eac24570689b7b
eeoooue <pdmoorhouse@gmail.com>
2025-02-18 19:32:15 +0000

    adopted new structure

---
+++ Solution/TestsRequirements/Objective01.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestsRequirements
+{
+    [TestClass]
+    public class Objective01
+    {
+        /// <summary>
+        /// ProducesValidSolution
+        /// </summary>
+        [TestMethod]
+        public void Req1x01()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        [TestMethod]
+        public void Req1x02()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        [TestMethod]
+        public void Req1x03()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
