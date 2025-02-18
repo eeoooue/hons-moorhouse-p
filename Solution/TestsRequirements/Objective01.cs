b8d9ddd557d27ad2cb555c492fefefba88aa951a
eeoooue <pdmoorhouse@gmail.com>
2025-02-18 21:06:03 +0000

    added exampleB alignment of 6 sequences

--- Solution/TestsRequirements/Objective01.cs
+++ Solution/TestsRequirements/Objective01.cs
@@ -1,34 +1,39 @@
-﻿using MAli;
+﻿using LibAlignment;
+using LibAlignment.Aligners.SingleState;
+using LibBioInfo;
+using LibBioInfo.ScoringMatrices;
+using LibScoring;
+using LibScoring.FitnessFunctions;
+using MAli;
 using MAli.AlignmentEngines;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TestsHarness;
+using TestsHarness.Tools;
 
 namespace TestsRequirements
 {
     [TestClass]
     public class Objective01
     {
+
+        private ExampleAlignments ExampleAlignments = Harness.ExampleAlignments;
+        private AlignmentConservation AlignmentConservation = Harness.AlignmentConservation;
+
         /// <summary>
         /// Given sequences to align, produces a valid solution - independent of quality.
         /// </summary>
         [DataTestMethod]
-        [DataRow("", "")]
-        public void Req1x01(string inputPath, string outputPath)
+        public void Req1x01()
         {
-            throw new NotImplementedException();
-
-            MAliInterface mali = new MAliInterface();
-
-            string command = $"-input {inputPath} -output {outputPath}";
-            string[] args = command.Split(' ');
-            mali.ProcessArguments(args);
-
-            bool alignmentProduced = File.Exists(outputPath);
-
-            Assert.IsTrue(alignmentProduced);
+            IterativeAligner aligner = GetAligner();
+            Alignment original = ExampleAlignments.GetExampleA();
+            List<BioSequence> sequences = original.Sequences;
+            Alignment alignment = aligner.AlignSequences(sequences);
+            AlignmentConservation.AssertAlignmentsAreConserved(alignment, original);
         }
 
         /// <summary>
@@ -44,21 +49,30 @@ namespace TestsRequirements
         /// Aligns sets of 6 typical protein sequences within 10 seconds on a university machine.
         /// </summary>
         [DataTestMethod]
-        [DataRow("", "")]
         [Timeout(10000)]
-        public void Req1x03(string inputPath, string outputPath)
+        public void Req1x03()
         {
-            throw new NotImplementedException();
+            IterativeAligner aligner = GetAligner();
+            List<BioSequence> sequences = GetTypicalSequences();
+            Assert.IsTrue(sequences.Count == 6);
+            Alignment alignment = aligner.AlignSequences(sequences);
+        }
 
-            MAliInterface mali = new MAliInterface();
+        private IterativeAligner GetAligner()
+        {
+            IScoringMatrix matrix = new PAM250Matrix();
+            IFitnessFunction objective = new SumOfPairsFitnessFunction(matrix);
+            IterativeAligner aligner = new IteratedLocalSearchAligner(objective, 100);
 
-            string command = $"-input {inputPath} -output {outputPath}";
-            string[] args = command.Split(' ');
-            mali.ProcessArguments(args);
+            return aligner;
+        }
 
-            bool alignmentProduced = File.Exists(outputPath);
+        private List<BioSequence> GetTypicalSequences()
+        {
+            Alignment original = ExampleAlignments.GetExampleB();
+            List<BioSequence> sequences = original.Sequences;
 
-            Assert.IsTrue(alignmentProduced);
+            return sequences;
         }
     }
 }
