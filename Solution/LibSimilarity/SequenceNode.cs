3f29aaf326bc9ecb562526312d4496a32d1f4aa2
eeoooue <pdmoorhouse@gmail.com>
2025-02-21 00:04:31 +0000

    created SequenceNode class

---
+++ Solution/LibSimilarity/SequenceNode.cs
@@ -0,0 +1,28 @@
+﻿using LibBioInfo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibSimilarity
+{
+    public class SequenceNode
+    {
+        public List<SimilarityLink> Connections = new List<SimilarityLink>();
+
+        public BioSequence Sequence;
+
+        public string Identifier { get { return Sequence.Identifier; } }
+
+        public SequenceNode(BioSequence sequence)
+        {
+            Sequence = sequence;
+        }
+
+        public void AddConnection(SimilarityLink link)
+        {
+            Connections.Add(link);
+        }
+    }
+}
