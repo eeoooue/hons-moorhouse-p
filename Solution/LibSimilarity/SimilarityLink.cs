51f2e2dbf71791755cd161368063d3073b77db5c
eeoooue <pdmoorhouse@gmail.com>
2025-02-21 00:04:46 +0000

    created SimilarityLink class

---
+++ Solution/LibSimilarity/SimilarityLink.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibSimilarity
+{
+    public class SimilarityLink
+    {
+        SequenceNode NodeA;
+        SequenceNode NodeB;
+        double SimilarityScore;
+
+        public SimilarityLink(SequenceNode a, SequenceNode b, double score)
+        {
+            NodeA = a;
+            NodeB = b;
+            SimilarityScore = score;
+        }
+    }
+}
