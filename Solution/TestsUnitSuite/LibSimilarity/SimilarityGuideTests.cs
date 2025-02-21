bc11fb201ba68477d0c33dcd9068f0b75c6037ff
eeoooue <pdmoorhouse@gmail.com>
2025-02-21 22:04:32 +0000

    added tests for SequenceNode & SimilarityGuide

--- Solution/TestsUnitSuite/LibSimilarity/SimilarityGuideTests.cs
+++ Solution/TestsUnitSuite/LibSimilarity/SimilarityGuideTests.cs
@@ -1,24 +1,45 @@
-﻿using System;
+﻿using LibBioInfo;
+using LibSimilarity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TestsHarness;
+using TestsHarness.Tools;
 
 namespace TestsUnitSuite.LibSimilarity
 {
     [TestClass]
     public class SimilarityGuideTests
     {
+        ExampleAlignments ExampleAlignments = Harness.ExampleAlignments;
+
         [TestMethod]
         public void CanRecordSimilarity()
         {
-            throw new NotImplementedException();
+            List<BioSequence> sequences = GetExampleSequences();
+            SimilarityGuide guide = new SimilarityGuide(sequences);
+            guide.RecordSimilarity(sequences[0], sequences[1], 200);
         }
 
         [TestMethod]
         public void CanGetSetOfSequences()
         {
-            throw new NotImplementedException();
+            List<BioSequence> sequences = GetExampleSequences();
+            SimilarityGuide guide = new SimilarityGuide(sequences);
+            guide.RecordSimilarity(sequences[0], sequences[1], 200);
+
+            List<BioSequence> set = guide.GetSetOfSimilarSequences();
+            Assert.IsTrue(set.Count > 0);
+        }
+
+        public List<BioSequence> GetExampleSequences()
+        {
+            Alignment alignment = ExampleAlignments.GetExampleA();
+            List<BioSequence> sequences = alignment.Sequences;
+
+            return sequences;
         }
     }
 }
