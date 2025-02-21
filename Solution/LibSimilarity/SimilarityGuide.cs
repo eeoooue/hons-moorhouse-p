74ac2e18029dca8c7a6f7aa6d88ae2bc3b782f94
eeoooue <pdmoorhouse@gmail.com>
2025-02-21 00:45:44 +0000

    scaffolded a SimilarityGuide

---
+++ Solution/LibSimilarity/SimilarityGuide.cs
@@ -0,0 +1,32 @@
+﻿using LibBioInfo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibSimilarity
+{
+    internal class SimilarityGuide
+    {
+        private SimilarityGraph Graph;
+
+        public SimilarityGuide(List<BioSequence> sequences)
+        {
+            Graph = new SimilarityGraph(sequences);
+        }
+
+        public void RecordSimilarity(BioSequence a, BioSequence b, double score)
+        {
+            Graph.RecordSimilarity(a, b, score);
+        }
+
+
+
+        public List<BioSequence> GetFuzzySet()
+        {
+
+        }
+
+    }
+}
