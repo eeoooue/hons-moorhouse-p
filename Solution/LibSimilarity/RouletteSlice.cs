0b1f6ed10fab82746cea7648bdcaf7dde11f0f10
eeoooue <pdmoorhouse@gmail.com>
2025-02-23 23:58:39 +0000

    added RouletteSlice class

---
+++ Solution/LibSimilarity/RouletteSlice.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibSimilarity
+{
+    public class RouletteSlice
+    {
+        public SimilarityLink Link;
+        public double Weighting;
+
+        public RouletteSlice(SimilarityLink link, double weighting)
+        {
+            Link = link;
+            Weighting = weighting;
+        }
+    }
+}
