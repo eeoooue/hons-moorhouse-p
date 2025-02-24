4cb1fc11d5c282a7f3edf39a4961ca128f41d7fc
eeoooue <pdmoorhouse@gmail.com>
2025-02-24 19:07:43 +0000

    Create ScoreRequest.cs

---
+++ Solution/MAli/UserRequests/ScoreRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAli.UserRequests
+{
+    public class ScoreRequest : UserRequest
+    {
+        public string InputPath = "";
+        public string OutputPath = "";
+
+        public bool SpecifiesCustomConfig = false;
+        public string ConfigPath = "";
+    }
+}
