4a5e67e4199b14f533e553e42525b661f84b4ea1
eeoooue <pdmoorhouse@gmail.com>
2025-02-25 16:51:35 +0000

    updated comment

--- Solution/LibModification/Mechanisms/GapInsertion.cs
+++ Solution/LibModification/Mechanisms/GapInsertion.cs
@@ -12,7 +12,7 @@ namespace LibModification.Mechanisms
     {
         public void InsertGaps(Alignment alignment, bool[] mask, int width, int j1, int j2)
         {
-            // j denotes where gaps start. so j can be 0 to n (alignment width)
+            // j values denote where gaps start. so j can be 0 to n (alignment width)
 
             char[,] matrix = InsertGaps(alignment.CharacterMatrix, mask, width, j1, j2);
             alignment.CharacterMatrix = matrix;
@@ -28,7 +28,6 @@ namespace LibModification.Mechanisms
             for (int i = 0; i < m; i++)
             {
                 int gapPosition = mask[i] ? j1 : j2;
-                // Console.WriteLine($"Inserting gap of width = {width} @ {gapPosition}");
                 PasteSequenceWithGapAt(original, result, i, gapPosition, width);
             }
 
@@ -53,47 +52,6 @@ namespace LibModification.Mechanisms
             {
                 destination[i, j + width] = source[i, j];
             }
-        }
-
-
-
-        public void OldPasteSequenceWithGapAt(in char[,] source, char[,] destination, int i, int gapPosition, int width)
-        {
-            string original = ReadMatrixRow(source, i);
-            string payload = InsertGapInPayload(original, width, gapPosition);
-
-            for(int j=0; j<payload.Length; j++)
-            {
-                destination[i, j] = payload[j];
-            }
-        }
-
-        public string ReadMatrixRow(in char[,] matrix, int i)
-        {
-            int n = matrix.GetLength(1);
-            StringBuilder sb = new StringBuilder();
-            for(int j=0; j<n; j++)
-            {
-                sb.Append(matrix[i, j]);
-            }
-
-            return sb.ToString();
-        }
-
-        public string InsertGapInPayload(string payload, int gapWidth, int position)
-        {
-            string before = payload.Substring(0, position);
-            string after = payload.Substring(position);
-
-            StringBuilder sb = new StringBuilder();
-            sb.Append(before);
-            for (int i = 0; i < gapWidth; i++)
-            {
-                sb.Append('-');
-            }
-            sb.Append(after);
-
-            return sb.ToString();
         }
     }
 }
