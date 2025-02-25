b5de268f2ff025455b5c5de1e585b35c1e51a978
eeoooue <pdmoorhouse@gmail.com>
2025-02-25 17:22:06 +0000

    scaffolded ColumnInsertion mechanism

--- Solution/LibModification/Mechanisms/ColumnInsertion.cs
+++ Solution/LibModification/Mechanisms/ColumnInsertion.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LibBioInfo;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,17 @@ namespace LibModification.Mechanisms
 {
     public static class ColumnInsertion
     {
+        public static void InsertEmptyColumn(Alignment alignment, int j)
+        {
+            char[,] matrix = InsertEmptyColumn(alignment.CharacterMatrix, j);
+            alignment.CharacterMatrix = matrix;
+        }
 
-
-
+        public static char[,] InsertEmptyColumn(char[,] matrix, int j)
+        {
+            int m = matrix.GetLength(0);
+            bool[] mask = new bool[m];
+            return GapInsertion.InsertGaps(matrix, mask, 1, j, j);
+        }
     }
 }
