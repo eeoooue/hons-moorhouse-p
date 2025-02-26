c832ecef51e8a6858a05b63bcbb6c80222cd669c
eeoooue <pdmoorhouse@gmail.com>
2025-02-26 22:41:07 +0000

    CrowdedComparisonOperator can sort tradeoffs

--- Solution/LibParetoAlignment/Helpers/CrowdedComparisonOperator.cs
+++ Solution/LibParetoAlignment/Helpers/CrowdedComparisonOperator.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +9,26 @@ namespace LibParetoAlignment.Helpers
 {
     public class CrowdedComparisonOperator
     {
+        public static void SortTradeoffs(List<TradeoffAlignment> tradeoffs)
+        {
+            int n = tradeoffs.Count;
+            for (int i = 0; i < n - 1; i++)
+            {
+                int currentMinIndex = i;
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (PreferAOverB(tradeoffs[j], tradeoffs[currentMinIndex]))
+                    {
+                        currentMinIndex = j;
+                    }
+                }
+
+                TradeoffAlignment temp = tradeoffs[i];
+                tradeoffs[i] = tradeoffs[currentMinIndex];
+                tradeoffs[currentMinIndex] = temp;
+            }
+        }
+
         public static bool PreferAOverB(TradeoffAlignment a, TradeoffAlignment b)
         {
             if (a.FrontRank < b.FrontRank)
