4559c4035dc8fcbae5c8695a6e15c681d5b812f8
eeoooue <pdmoorhouse@gmail.com>
2025-02-26 22:30:01 +0000

    scaffolded CrowdedComparisonOperator

---
+++ Solution/LibParetoAlignment/Helpers/CrowdedComparisonOperator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibParetoAlignment.Helpers
+{
+    public class CrowdedComparisonOperator
+    {
+        public static bool PreferAOverB(TradeoffAlignment a, TradeoffAlignment b)
+        {
+            if (a.FrontRank < b.FrontRank)
+            {
+                return true;
+            }
+
+            if (a.FrontRank > b.FrontRank)
+            {
+                return false;
+            }
+
+            return a.CrowdingDistance < b.CrowdingDistance;
+        }
+    }
+}
