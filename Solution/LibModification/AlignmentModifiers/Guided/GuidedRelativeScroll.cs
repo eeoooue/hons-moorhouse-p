e34486f70cfd46ceba73b95b524cc19e43d2f174
eeoooue <pdmoorhouse@gmail.com>
2025-02-26 00:13:33 +0000

    started a GuidedRelativeScroll modifier

---
+++ Solution/LibModification/AlignmentModifiers/Guided/GuidedRelativeScroll.cs
@@ -0,0 +1,19 @@
+﻿using LibBioInfo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibModification.AlignmentModifiers.Guided
+{
+    public class GuidedRelativeScroll : AlignmentModifier
+    {
+
+
+        public override char[,] GetModifiedAlignmentState(Alignment alignment)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
