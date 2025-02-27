87bcf74ef10f2612c0717f1cb2b1864037b26ac2
eeoooue <pdmoorhouse@gmail.com>
2025-02-27 12:59:18 +0000

    created MaskedAlignment abstraction

---
+++ Solution/LibModification/BlockShuffling/MaskedAlignment.cs
@@ -0,0 +1,25 @@
+﻿using LibBioInfo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibModification.BlockShuffling
+{
+    public class MaskedAlignment
+    {
+        public Alignment Alignment;
+        public bool[,] Mask;
+        public bool ResidueMarker;
+        public bool GapMarker;
+
+        public MaskedAlignment(Alignment alignment, bool[,] mask, bool residuesAsOnes = true)
+        {
+            Alignment = alignment;
+            Mask = mask;
+            ResidueMarker = residuesAsOnes;
+            GapMarker = !ResidueMarker;
+        }
+    }
+}
