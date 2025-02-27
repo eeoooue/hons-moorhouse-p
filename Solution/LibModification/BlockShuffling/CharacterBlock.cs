dd21f5fa1da3df53ef02189eb13e155ecb2c7d75
eeoooue <pdmoorhouse@gmail.com>
2025-02-27 13:06:14 +0000

    Create CharacterBlock.cs

---
+++ Solution/LibModification/BlockShuffling/CharacterBlock.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibModification.BlockShuffling
+{
+    internal class CharacterBlock
+    {
+        public List<int> SequenceIndices;
+
+        public bool[,] Mask;
+
+        public int Width;
+
+        public CharacterBlock(List<int> sequences, bool[,] mask)
+        {
+            SequenceIndices = sequences;
+            Mask = mask;
+            Width = Mask.GetLength(1);
+        }
+    }
+}
