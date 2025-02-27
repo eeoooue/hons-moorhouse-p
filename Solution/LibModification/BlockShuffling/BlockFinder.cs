37d628255f2933c5624f5bbb95c2f2b41a9577fe
eeoooue <pdmoorhouse@gmail.com>
2025-02-27 14:27:41 +0000

    continued drafting logic for BlockFinder

--- Solution/LibModification/BlockShuffling/BlockFinder.cs
+++ Solution/LibModification/BlockShuffling/BlockFinder.cs
@@ -11,18 +11,15 @@ namespace LibModification.BlockShuffling
     {
         public int Start = 0;
         public int End = 0;
+        public int WidthGoal = 0;
 
-        public PaddingInstructions(int start, int end)
+        public PaddingInstructions(int start, int end, int widthGoal)
         {
             Start = start;
             End = end;
+            WidthGoal = widthGoal;
         }
     }
-
-
-
-
-
 
     internal class BlockFinder
     {
@@ -33,21 +30,57 @@ namespace LibModification.BlockShuffling
             List<int> startPositions = new List<int>();
             List<int> endPositions = new List<int>();
             FindBlock(alignment, ref sequences, ref startPositions, ref endPositions);
-            return CreateBlock(alignment, sequences, startPositions, endPositions);
+            List<int> sequenceIndices = GetMaskAsListOfIndices(sequences);
+
+            return CreateBlock(alignment, sequenceIndices, startPositions, endPositions);
         }
 
-        public CharacterBlock CreateBlock(in MaskedAlignment alignment, in bool[] sequences, in List<int> startPositions, in List<int> endPositions)
+        public CharacterBlock CreateBlock(in MaskedAlignment alignment, in List<int> sequenceIndices, in List<int> startPositions, in List<int> endPositions)
         {
             int n = startPositions.Count;
             if (n != endPositions.Count)
             {
                 throw new Exception($"Number of start positions (x{startPositions.Count}) & end positions (x{endPositions.Count}) were unequal.");
             }
+            if (n != sequenceIndices.Count)
+            {
+                throw new Exception($"Number of start positions (x{startPositions.Count}) & sequence indices (x{sequenceIndices.Count}) were unequal.");
+            }
 
-            int earliestStart = startPositions[0];
-            int latestEnd = endPositions[0];
+            IdentifyEarliestStartAndLatestEnd(startPositions, endPositions, out int earliestStart, out int latestEnd);
 
-            for(int i=0; i<n; i++)
+            List<bool[]> reads = CollectPaddedReads(alignment, sequenceIndices, startPositions, endPositions);
+
+            return new CharacterBlock(earliestStart, sequenceIndices, reads);
+        }
+
+        public List<bool[]> CollectPaddedReads(in MaskedAlignment alignment, in List<int> sequenceIndices, in List<int> startPositions, in List<int> endPositions)
+        {
+            IdentifyEarliestStartAndLatestEnd(startPositions, endPositions, out int earliestStart, out int latestEnd);
+
+            int blockWidth = 1 + latestEnd - earliestStart;
+
+            List<bool[]> result = new List<bool[]>();
+
+            for (int i = 0; i < sequenceIndices.Count; i++)
+            {
+                int seqIndex = sequenceIndices[i];
+                int startPos = startPositions[i];
+                int endPos = startPositions[i];
+                PaddingInstructions padding = CalculatePadding(startPos, endPos, earliestStart, latestEnd);
+                bool[] paddedRead = GetPaddedRead(alignment, seqIndex, startPos, endPos, padding);
+                result.Add(paddedRead);
+            }
+
+            return result;
+        }
+
+        public void IdentifyEarliestStartAndLatestEnd(in List<int> startPositions, in List<int> endPositions, out int earliestStart, out int latestEnd)
+        {
+            earliestStart = startPositions[0];
+            latestEnd = endPositions[0];
+
+            for (int i = 0; i < startPositions.Count; i++)
             {
                 int start = startPositions[i];
                 earliestStart = Math.Min(earliestStart, start);
@@ -55,44 +88,36 @@ namespace LibModification.BlockShuffling
                 int end = endPositions[i];
                 latestEnd = Math.Max(latestEnd, end);
             }
-
-            int blockWidth = 1 + latestEnd - earliestStart;
-
-
-
-
-
-            throw new NotImplementedException();
         }
 
         public PaddingInstructions CalculatePadding(int jStart, int jEnd, int earliestStart, int latestEnd)
         {
             int startPadding = jStart - earliestStart;
             int endPadding = latestEnd - jEnd;
+            int blockWidth = 1 + latestEnd - earliestStart;
 
-            return new PaddingInstructions(startPadding, endPadding);
+            return new PaddingInstructions(startPadding, endPadding, blockWidth);
         }
 
-        public string GetPaddedRead(MaskedAlignment alignment, int i, int jStart, int jEnd, PaddingInstructions padding)
+        public bool[] GetPaddedRead(MaskedAlignment alignment, int i, int jStart, int jEnd, PaddingInstructions padding)
         {
-            StringBuilder sb = new StringBuilder();
+            bool[] result = new bool[padding.WidthGoal];
 
+            int p = 0;
             for(int r = 0; r<padding.Start; r++)
             {
-                sb.Append('-');
+                result[p++] = false;
             }
-
             for(int j=jStart; j<=jEnd; j++)
             {
-                sb.Append(alignment.Mask[i, j]);
+                result[p++] = alignment.Mask[i, j];
             }
-
             for (int r = 0; r < padding.End; r++)
             {
-                sb.Append('-');
+                result[p++] = false;
             }
 
-            return sb.ToString();
+            return result;
         }
 
         public void FindBlock(MaskedAlignment alignment, ref bool[] sequences, ref List<int> startPositions, ref List<int> endPositions)
@@ -117,11 +142,6 @@ namespace LibModification.BlockShuffling
                 startPositions.Add(jFrontInclusive);
                 endPositions.Add(jEndInclusive);
             }
-        }
-
-        public bool HasBlockStarter(MaskedAlignment alignment, int i, int j)
-        {
-            return alignment.Mask[i, j];
         }
 
         public void ExpandBlockWidth(MaskedAlignment alignment, int i, int jStart, out int jFrontInclusive, out int jEndInclusive)
