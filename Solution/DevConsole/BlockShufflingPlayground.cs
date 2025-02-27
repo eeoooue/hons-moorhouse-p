6c27b7c35dfa22e12880d8ac6f223774fdf9ca92
eeoooue <pdmoorhouse@gmail.com>
2025-02-27 15:56:32 +0000

    scaffolded print mask method

--- Solution/DevConsole/BlockShufflingPlayground.cs
+++ Solution/DevConsole/BlockShufflingPlayground.cs
@@ -1,5 +1,6 @@
 ﻿using LibBioInfo;
 using LibFileIO;
+using LibModification.BlockShuffling;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,18 +15,34 @@ namespace DevConsole
         public void Main()
         {
             FileHelper helper = new FileHelper();
-            Alignment alignment = helper.ReadAlignmentFrom("BB11001");
+            Alignment alignment = helper.ReadAlignmentFrom("clustalformat_BB11001.aln");
 
 
+            AlignmentMaskMaker maker = new AlignmentMaskMaker();
 
-
+            MaskedAlignment maskedAli = maker.GetMaskedAlignment(alignment);
+            PrintMask(maskedAli);
 
         }
 
+        public void PrintMask(MaskedAlignment maskedAli)
+        {
+            for (int i = 0; i < maskedAli.Height; i++)
+            {
+                PrintMaskRow(maskedAli.Mask, i);
+            }
+        }
 
+        public void PrintMaskRow(bool[,] mask, int i)
+        {
+            int n = mask.GetLength(1);
 
-
-
-
+            for(int j=0; j<n; j++)
+            {
+                char x = mask[i, j] ? 'X' : '-';
+                Console.Write(x);
+            }
+            Console.WriteLine();
+        }
     }
 }
