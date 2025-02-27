8b7119407f4bdd54e0c40bf05399dc2c370f3770
eeoooue <pdmoorhouse@gmail.com>
2025-02-27 13:54:26 +0000

    scaffolded mechanism for padded readings of blocks from alignment mask

--- Solution/LibModification/BlockShuffling/BlockFinder.cs
+++ Solution/LibModification/BlockShuffling/BlockFinder.cs
@@ -7,9 +7,93 @@ using System.Threading.Tasks;
 
 namespace LibModification.BlockShuffling
 {
+    internal struct PaddingInstructions
+    {
+        public int Start = 0;
+        public int End = 0;
+
+        public PaddingInstructions(int start, int end)
+        {
+            Start = start;
+            End = end;
+        }
+    }
+
+
+
+
+
+
     internal class BlockFinder
     {
         // aims to find a block of ones based on logic described in SAGA
+
+        public CharacterBlock FindBlock(MaskedAlignment alignment, ref bool[] sequences)
+        {
+            List<int> startPositions = new List<int>();
+            List<int> endPositions = new List<int>();
+            FindBlock(alignment, ref sequences, ref startPositions, ref endPositions);
+            return CreateBlock(alignment, sequences, startPositions, endPositions);
+        }
+
+        public CharacterBlock CreateBlock(in MaskedAlignment alignment, in bool[] sequences, in List<int> startPositions, in List<int> endPositions)
+        {
+            int n = startPositions.Count;
+            if (n != endPositions.Count)
+            {
+                throw new Exception($"Number of start positions (x{startPositions.Count}) & end positions (x{endPositions.Count}) were unequal.");
+            }
+
+            int earliestStart = startPositions[0];
+            int latestEnd = endPositions[0];
+
+            for(int i=0; i<n; i++)
+            {
+                int start = startPositions[i];
+                earliestStart = Math.Min(earliestStart, start);
+
+                int end = endPositions[i];
+                latestEnd = Math.Max(latestEnd, end);
+            }
+
+            int blockWidth = 1 + latestEnd - earliestStart;
+
+
+
+
+
+            throw new NotImplementedException();
+        }
+
+        public PaddingInstructions CalculatePadding(int jStart, int jEnd, int earliestStart, int latestEnd)
+        {
+            int startPadding = jStart - earliestStart;
+            int endPadding = latestEnd - jEnd;
+
+            return new PaddingInstructions(startPadding, endPadding);
+        }
+
+        public string GetPaddedRead(MaskedAlignment alignment, int i, int jStart, int jEnd, PaddingInstructions padding)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for(int r = 0; r<padding.Start; r++)
+            {
+                sb.Append('-');
+            }
+
+            for(int j=jStart; j<=jEnd; j++)
+            {
+                sb.Append(alignment.Mask[i, j]);
+            }
+
+            for (int r = 0; r < padding.End; r++)
+            {
+                sb.Append('-');
+            }
+
+            return sb.ToString();
+        }
 
         public void FindBlock(MaskedAlignment alignment, ref bool[] sequences, ref List<int> startPositions, ref List<int> endPositions)
         {
