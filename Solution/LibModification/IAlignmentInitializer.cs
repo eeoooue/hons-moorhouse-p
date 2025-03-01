60fc3828d1841e763732e4fd3fbd654f3590f6f6
eeoooue <pdmoorhouse@gmail.com>
2025-03-01 23:05:18 +0000

    created IAlignmentInitializer interface

---
+++ Solution/LibModification/IAlignmentInitializer.cs
@@ -0,0 +1,14 @@
+﻿using LibBioInfo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibModification
+{
+    public interface IAlignmentInitializer
+    {
+        public Alignment CreateInitialAlignment(List<BioSequence> sequences);
+    }
+}
