a8cd827ba66ca1c9915c52422be859c7fbdc592b
eeoooue <pdmoorhouse@gmail.com>
2025-03-02 20:18:33 +0000

    now using the Sprint07 config

--- Solution/MAli/MAliFacade.cs
+++ Solution/MAli/MAliFacade.cs
@@ -17,7 +17,7 @@ namespace MAli
 {
     public class MAliFacade
     {
-        public AlignmentConfig Config = new DevelopmentConfig();
+        public AlignmentConfig Config = new Sprint07Config();
         public ParetoAlignmentConfig ParetoConfig = new ParetoDevConfig();
 
         public void CheckSetSeed(AlignmentRequest request)
