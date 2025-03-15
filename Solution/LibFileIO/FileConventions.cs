05477ee5d2d5329c1f1c231f08d5dabeeaf4946c
eeoooue <pdmoorhouse@gmail.com>
2025-03-15 17:36:31 +0000

    Create FileConventions.cs

---
+++ Solution/LibFileIO/FileConventions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibFileIO
+{
+    public static class FileConventions
+    {
+        private static HashSet<char> GapCharacters = new HashSet<char> { '-', '.' };
+        private static HashSet<char> GapChars = new HashSet<char> { '-', '.' };
+
+        public static bool IsGap(char c)
+        {
+            return GapCharacters.Contains(c);
+        }
+
+        public static bool IsGapChar(char c)
+        {
+            return GapCharacters.Contains(c);
+        }
+    }
+}
